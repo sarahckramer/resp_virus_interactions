68da6d3229d5e5059d09a391cdc9cc340433a432
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-08-06 10:16:42 +0200

    Clean up comparison code

--- src/checks/compare_individual_seasons_vs_global.R
+++ src/checks/compare_individual_seasons_vs_global.R
@@ -1,6 +1,5 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# Code to confirm that using global likelihood with no global parameters can achieve the same results as fitting
-# each season separately (flu_B only)
+# Compare estimates of season-specific parameters, as well as global parameters, under different fitting schemes
 # ---------------------------------------------------------------------------------------------------------------------
 
 # Setup
@@ -132,17 +131,17 @@ res_glob <- pars_top %>%
   mutate(year = str_sub(param, 1, 4),
          param = str_sub(param, 6, str_length(param))) %>%
   select(param:year) %>%
-  mutate(method = 'Global_p1')
+  mutate(method = 'Global_p1_1')
 
 # Combine data frames:
 res_df <- res_df %>%
   bind_rows(res_glob)
 
-# Plot results:
-p2 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
-  facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
-print(p2)
-# pretty much the same values for all of these parameters regardless of theta_lambda1 value
+# # Plot results:
+# p2 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
+#   facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
+# print(p2)
+# # pretty much the same values for all of these parameters regardless of theta_lambda1 value
 
 # Explore correlations:
 pars_corr <- pars_top %>%
@@ -173,79 +172,6 @@ pairs(pars_corr, pch = 20)
 # 
 # plot(slices$theta_lambda1, slices$ll, type = 'l', xlab = 'theta_lambda1', ylab = 'Log-Likelihood')
 # # fits to wildly different values depending on very slight changes in the values for other parameters
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-# Compile estimates of theta_lambda1/delta
-
-# Set estpars:
-shared_estpars <- c('theta_lambda1', 'delta')
-unit_estpars <- c('Ri1', 'Ri2', 'I10', 'I20', 'R10', 'R20', 'R120')
-
-true_estpars <- c(shared_estpars, unit_estpars)
-
-# Get list of results files:
-res_files <- list.files(path = 'results/030821_fluB_2params/', full.names = TRUE)
-
-# Read in results:
-res_full = list()
-for (i in seq_along(res_files)) {
-  res_full[[i]] <- read_rds(res_files[[i]])
-}
-rm(i)
-
-# Get parameter estimates and log-likelihoods:
-pars_df <- lapply(res_full, getElement, 'estpars') %>%
-  bind_rows() %>%
-  bind_cols('loglik' = lapply(res_full, getElement, 'll') %>%
-              unlist())
-expect_true(nrow(pars_df) == length(res_files))
-expect_true(all(is.finite(pars_df$loglik)))
-
-# Keep only top results:
-pars_df <- pars_df %>%
-  arrange(desc(loglik))
-
-no_best <- nrow(subset(pars_df, 2 * (max(loglik) - loglik) <= qchisq(p = 0.99, df = (dim(pars_df)[2] - 1))))
-no_best <- max(no_best, 50)
-print(no_best)
-
-pars_top <- pars_df[1:no_best, ]
-
-# Clean up:
-rm(res_files, res_full, no_best)
-
-# Compare:
-res_glob <- pars_top %>%
-  pivot_longer(-c(theta_lambda1, delta, loglik), names_to = 'param') %>%
-  mutate(year = str_sub(param, 1, 4),
-         param = str_sub(param, 6, str_length(param))) %>%
-  select(param:year) %>%
-  mutate(method = 'Global_p2')
-
-# Combine data frames:
-res_df <- res_df %>%
-  bind_rows(res_glob)
-
-# Plot results:
-p3 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
-  facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
-print(p3)
-# still very similar
-
-# Explore correlations:
-pars_corr <- pars_top %>%
-  pivot_longer(-c(theta_lambda1, delta, loglik), names_to = 'param') %>%
-  mutate(year = str_sub(param, 1, 4),
-         param = str_sub(param, 6, str_length(param))) %>%
-  pivot_wider(names_from = param, values_from = value) %>%
-  select(-year)
-pairs(pars_corr, pch = 20)
-
-# Look at theta_lambda1 and delta values:
-pars_top %>% filter(theta_lambda1 < 1.0) %>% pull(theta_lambda1) %>% summary()
-pars_top %>% filter(theta_lambda1 > 1.0) %>% pull(theta_lambda1) %>% summary()
-pars_top %>% filter(delta > 0.117) %>% pull(delta)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -300,11 +226,11 @@ res_glob <- pars_top %>%
 res_df <- res_df %>%
   bind_rows(res_glob)
 
-# Plot results:
-p4 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
-  facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
-print(p4)
-# similar, but appear to have wider intervals?
+# # Plot results:
+# p3 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
+#   facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
+# print(p3)
+# # similar, but appear to have wider intervals?
 
 # Explore correlations:
 pars_corr <- pars_top %>%
@@ -441,9 +367,9 @@ res_df <- res_df %>%
   bind_rows(res_glob)
 
 # Plot results:
-p5 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
+p4 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
   facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
-print(p5)
+print(p4)
 # similar; narrower intervals than when using global search
 
 # Explore correlations:
@@ -531,112 +457,107 @@ rm(list = ls())
 # ---------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------
 
-# For flu_A
-
-# Set virus 1:
-vir1 <- 'flu_A'
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-# Get results from fitting individual seasons
-
-# Read in results:
-res_ind <- read_rds('results/traj_match_round1_byvirseas_TOP.rds')
-
-# Compile to data frame:
-res_ind <- bind_rows(res_ind)
-
-# Keep flu_A only:
-res_ind <- res_ind %>%
-  filter(virus1 == 'flu_A')
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-# Compile estimates of theta_lambda1/delta
-
-# Set estpars:
-shared_estpars <- c('theta_lambda1', 'delta')
-unit_estpars <- c('Ri1', 'Ri2', 'I10', 'I20', 'R10', 'R20', 'R120')
-
-true_estpars <- c(shared_estpars, unit_estpars)
-
-# Get list of results files:
-res_files <- list.files(path = 'results/030821_fluA_2params/', full.names = TRUE)
-
-# Read in results:
-res_full = list()
-for (i in seq_along(res_files)) {
-  res_full[[i]] <- read_rds(res_files[[i]])
-}
-rm(i)
-
-# Get parameter estimates and log-likelihoods:
-pars_df <- lapply(res_full, getElement, 'estpars') %>%
-  bind_rows() %>%
-  bind_cols('loglik' = lapply(res_full, getElement, 'll') %>%
-              unlist())
-expect_true(nrow(pars_df) == length(res_files))
-expect_true(all(is.finite(pars_df$loglik)))
-
-# Keep only top results:
-pars_df <- pars_df %>%
-  arrange(desc(loglik))
-
-no_best <- nrow(subset(pars_df, 2 * (max(loglik) - loglik) <= qchisq(p = 0.99, df = (dim(pars_df)[2] - 1))))
-no_best <- max(no_best, 50)
-print(no_best)
-
-pars_top <- pars_df[1:no_best, ]
-
-# Clean up:
-rm(res_files, res_full, no_best)
-
-# Compare:
-res_glob <- pars_top %>%
-  pivot_longer(-c(theta_lambda1, delta, loglik), names_to = 'param') %>%
-  mutate(year = str_sub(param, 1, 4),
-         param = str_sub(param, 6, str_length(param))) %>%
-  select(param:year) %>%
-  mutate(method = 'Global_p2')
-
-# Combine data frames:
-res_df <- res_ind %>%
-  select(year:R120) %>%
-  pivot_longer(-year, names_to = 'param') %>%
-  mutate(method = 'Seasonal') %>%
-  bind_rows(res_glob)
-
-# Plot results:
-p1 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
-  facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
-print(p1)
-# similar values
-
-# Explore correlations:
-pars_corr <- pars_top %>%
-  pivot_longer(-c(theta_lambda1, delta, loglik), names_to = 'param') %>%
-  mutate(year = str_sub(param, 1, 4),
-         param = str_sub(param, 6, str_length(param))) %>%
-  pivot_wider(names_from = param, values_from = value) %>%
-  select(-year)
-pairs(pars_corr, pch = 20)
-
-# Check theta_lambda1/delta values:
-summary(pars_top$theta_lambda1)
-summary(pars_top$delta)
-
-pars_top %>% filter(theta_lambda1 < 1.0 & theta_lambda1 > 0.01) %>% pull(theta_lambda1) %>% summary()
-pars_top %>% filter(theta_lambda1 > 1.0) %>% pull(theta_lambda1) %>% summary()
-pars_top %>% filter(delta > 0.117) %>% pull(delta)
-
-pars_top %>% filter(delta > 0.117) # no good, realistic combinations of estimates
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-
-
-# ---------------------------------------------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------------------------------------------
+# # For flu_A
+# 
+# # Set virus 1:
+# vir1 <- 'flu_A'
+# 
+# # ---------------------------------------------------------------------------------------------------------------------
+# 
+# # Get results from fitting individual seasons
+# 
+# # Read in results:
+# res_ind <- read_rds('results/traj_match_round1_byvirseas_TOP.rds')
+# 
+# # Compile to data frame:
+# res_ind <- bind_rows(res_ind)
+# 
+# # Keep flu_A only:
+# res_ind <- res_ind %>%
+#   filter(virus1 == 'flu_A')
+# 
+# # ---------------------------------------------------------------------------------------------------------------------
+# 
+# # Compile estimates of theta_lambda1/delta
+# 
+# # Set estpars:
+# shared_estpars <- c('theta_lambda1', 'delta')
+# unit_estpars <- c('Ri1', 'Ri2', 'I10', 'I20', 'R10', 'R20', 'R120')
+# 
+# true_estpars <- c(shared_estpars, unit_estpars)
+# 
+# # Get list of results files:
+# res_files <- list.files(path = 'results/030821_fluA_2params/', full.names = TRUE)
+# 
+# # Read in results:
+# res_full = list()
+# for (i in seq_along(res_files)) {
+#   res_full[[i]] <- read_rds(res_files[[i]])
+# }
+# rm(i)
+# 
+# # Get parameter estimates and log-likelihoods:
+# pars_df <- lapply(res_full, getElement, 'estpars') %>%
+#   bind_rows() %>%
+#   bind_cols('loglik' = lapply(res_full, getElement, 'll') %>%
+#               unlist())
+# expect_true(nrow(pars_df) == length(res_files))
+# expect_true(all(is.finite(pars_df$loglik)))
+# 
+# # Keep only top results:
+# pars_df <- pars_df %>%
+#   arrange(desc(loglik))
+# 
+# no_best <- nrow(subset(pars_df, 2 * (max(loglik) - loglik) <= qchisq(p = 0.99, df = (dim(pars_df)[2] - 1))))
+# no_best <- max(no_best, 50)
+# print(no_best)
+# 
+# pars_top <- pars_df[1:no_best, ]
+# 
+# # Clean up:
+# rm(res_files, res_full, no_best)
+# 
+# # Compare:
+# res_glob <- pars_top %>%
+#   pivot_longer(-c(theta_lambda1, delta, loglik), names_to = 'param') %>%
+#   mutate(year = str_sub(param, 1, 4),
+#          param = str_sub(param, 6, str_length(param))) %>%
+#   select(param:year) %>%
+#   mutate(method = 'Global_p2')
+# 
+# # Combine data frames:
+# res_df <- res_ind %>%
+#   select(year:R120) %>%
+#   pivot_longer(-year, names_to = 'param') %>%
+#   mutate(method = 'Seasonal') %>%
+#   bind_rows(res_glob)
+# 
+# # Plot results:
+# p1 <- ggplot(data = res_df, aes(x = year, y = value, fill = method)) + geom_boxplot() +
+#   facet_wrap(~param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
+# print(p1)
+# # similar values
+# 
+# # Explore correlations:
+# pars_corr <- pars_top %>%
+#   pivot_longer(-c(theta_lambda1, delta, loglik), names_to = 'param') %>%
+#   mutate(year = str_sub(param, 1, 4),
+#          param = str_sub(param, 6, str_length(param))) %>%
+#   pivot_wider(names_from = param, values_from = value) %>%
+#   select(-year)
+# pairs(pars_corr, pch = 20)
+# 
+# # Check theta_lambda1/delta values:
+# summary(pars_top$theta_lambda1)
+# summary(pars_top$delta)
+# 
+# pars_top %>% filter(theta_lambda1 < 1.0 & theta_lambda1 > 0.01) %>% pull(theta_lambda1) %>% summary()
+# pars_top %>% filter(theta_lambda1 > 1.0) %>% pull(theta_lambda1) %>% summary()
+# pars_top %>% filter(delta > 0.117) %>% pull(delta)
+# 
+# pars_top %>% filter(delta > 0.117) # no good, realistic combinations of estimates
+# 
+# # ---------------------------------------------------------------------------------------------------------------------
 
 # Clean up:
 rm(list = ls())
