b6e4d61b14009a8ab814f15bda7b17eac6b64fd8
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-11-01 14:23:46 +0100

    Format HK data for other respiratory viruses

--- src/format_data/format_HK_data.R
+++ src/format_data/format_HK_data.R
@@ -11,9 +11,9 @@ library(gridExtra)
 years <- 2014:2021
 
 # Read in data:
-file_list_flu <- list.files(path = 'data/raw/', pattern = 'flu', full.names = TRUE)
-file_list_rsv <- list.files(path = 'data/raw/', pattern = 'rsv', full.names = TRUE)
-file_list_ili <- list.files(path = 'data/raw/', pattern = 'ili', full.names = TRUE)
+file_list_flu <- list.files(path = 'data/raw/hk/', pattern = 'flu', full.names = TRUE)
+file_list_rsv <- list.files(path = 'data/raw/hk/', pattern = 'rsv', full.names = TRUE)
+file_list_ili <- list.files(path = 'data/raw/hk/', pattern = 'ili', full.names = TRUE)
 
 data_list_flu = data_list_rsv = data_list_ili = vector('list', length = length(file_list_flu))
 for (i in 1:length(file_list_flu)) {
@@ -46,13 +46,45 @@ data_list_flu <- lapply(data_list_flu, function(ix) {
 
 # Format RSV data:
 data_list_rsv <- lapply(data_list_rsv, function(ix) {
-  ix[-1, ] %>%
-    rename('Week' = 'Week number',
-           'n_samp' = 'No. of specimen tested',
-           'n_rsv' = 'RSV') %>%
-    # select(13, 1, 3, 6) %>%
-    select(Year, Week, n_samp, n_rsv) %>%
+  
+  names(ix) <- make.names(names(ix))
+  if (!('Rhinovirus...Enterovirus' %in% names(ix))) {
+    ix <- ix %>%
+      rename('Rhinovirus...Enterovirus' = 'X.Rhinovirus...Enterovirus')
+  }
+  
+  if ('Human.metapneumovirus' %in% names(ix)) {
+    ix <- ix %>%
+      rename('n_hmpv' = 'Human.metapneumovirus')
+  } else if ('Human.metapneumovirus.' %in% names(ix)) {
+    ix <- ix %>%
+      rename('n_hmpv' = 'Human.metapneumovirus.')
+  }
+  
+  locate_diff <- ix[1, ] %>% unlist() %>% as.vector()
+  locate_diff <- which(locate_diff == 'Differentiation performed')
+  select_cols <- paste0('X', seq(locate_diff, locate_diff + 2))
+  
+  ix <- ix[-1, ] %>%
+    rename('Week' = 'Week.number',
+           'n_samp' = 'No..of.specimen.tested',
+           'n_rsv' = 'RSV',
+           'n_adeno' = 'Adenovirus',
+           'n_rhino_entero' = 'Rhinovirus...Enterovirus')
+  
+  if (!('n_hmpv' %in% names(ix))) {
+    ix <- ix %>%
+      mutate(n_hmpv = NA)
+  }
+  
+  ix <- ix %>%
+    select(Year, Week, n_samp, n_rsv, n_adeno, n_hmpv, n_rhino_entero, all_of(select_cols)) %>%
+    rename('n_diff' = select_cols[1],
+           'n_rhino' = select_cols[2],
+           'n_entero' = select_cols[3]) %>%
     mutate(across(.cols = everything(), as.numeric))
+  
+  ix
 })
 # Starting in 2020 week 8, ages are only <18!
 
@@ -93,12 +125,20 @@ dat_hk <- dat_hk %>%
   mutate(Time = 1:nrow(dat_hk)) %>%
   filter(Time < Time[Year == 2020 & Week == 8])
 
+# Estimate number of rhino/entero cases based on proportion of differentiated samples that are rhino vs. entero:
+dat_hk <- dat_hk %>%
+  mutate(n_rhino_est = (n_rhino / n_diff) * n_rhino_entero,
+         n_entero_est = (n_entero / n_diff) * n_rhino_entero,
+         n_rhino_est_rnd = round(n_rhino_est),
+         n_entero_est_rnd = round(n_entero_est)) %>%
+  select(Time, Year:n_rhino_entero, n_rhino_est:n_entero_est_rnd, GOPC:PMP.Clinics)
+
 # Plot:
-p1 <- ggplot(data = dat_hk %>% pivot_longer(n_h1:n_rsv, names_to = 'vir', values_to = 'val'),
+p1 <- ggplot(data = dat_hk %>% pivot_longer(c(n_h1:n_rsv, n_rhino_est), names_to = 'vir', values_to = 'val'),
              aes(x = Time, y = val, color = vir)) + geom_line() + theme_classic() + #facet_wrap(~ vir) +
   labs(x = 'Time', y = '# Samples Pos', color = 'Virus') +
   scale_x_continuous(breaks = c(1, 53, 105, 158, 210, 262, 314, 366), labels = years)
-p2 <- ggplot(data = dat_hk %>% pivot_longer(n_h1:n_rsv, names_to = 'vir', values_to = 'val'),
+p2 <- ggplot(data = dat_hk %>% pivot_longer(c(n_h1:n_rsv, n_rhino_est), names_to = 'vir', values_to = 'val'),
              aes(x = Time, y = val / n_samp, color = vir)) + geom_line() + theme_classic() +
   labs(x = 'Time', y = '% Samples Pos', color = 'Virus') +
   scale_x_continuous(breaks = c(1, 53, 105, 158, 210, 262, 314, 366), labels = years) +
@@ -109,3 +149,22 @@ p3 <- ggplot(data = dat_hk %>% pivot_longer(GOPC:PMP.Clinics, names_to = 'clinic
   scale_x_continuous(breaks = c(1, 53, 105, 158, 210, 262, 314, 366), labels = years) +
   theme(legend.position = 'bottom')
 grid.arrange(p2, p3, ncol = 1)
+
+p4 <- ggplot(data = dat_hk %>% pivot_longer(c(n_h1:n_hmpv, n_rhino_est:n_entero_est), names_to = 'vir', values_to = 'val'),
+       aes(x = Time, y = val / n_samp, color = vir)) + geom_line() + theme_classic() +
+  labs(x = 'Time', y = '% Samples Pos', color = 'Virus') + facet_wrap(~ vir) +
+  scale_x_continuous(breaks = c(1, 53, 105, 158, 210, 262, 314, 366), labels = years) +
+  theme(legend.position = 'none')
+print(p4)
+
+p5 <- ggplot(data = dat_hk, aes(x = Time, y = n_samp)) + geom_line() +
+  theme_classic() + labs(x = 'Time', y = '# of Vir. Samples') +
+  scale_x_continuous(breaks = c(1, 53, 105, 158, 210, 262, 314, 366), labels = years)
+print(p5)
+
+# Output to pdf:
+pdf('results/plots/data_Hong_Kong.pdf', width = 10, height = 6)
+grid.arrange(p2, p3, ncol = 1)
+print(p4)
+print(p5)
+dev.off()
