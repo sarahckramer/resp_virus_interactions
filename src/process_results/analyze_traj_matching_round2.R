3d0a1ff05ef9a4100e1b54c4f725f91e84b8e039
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-11-22 17:30:23 +0100

    Get results when interactions in both directions are fit

--- src/process_results/analyze_traj_matching_round2.R
+++ src/process_results/analyze_traj_matching_round2.R
@@ -76,36 +76,36 @@ load_and_format_mega_results <- function(filepath, shared_estpars, unit_estpars,
 # Read in and format results for all runs
 
 # Set shared and unit parameters:
-shared_estpars <- c('rho1', 'rho2', 'delta', 'theta_lambda1')
+shared_estpars <- c('rho1', 'rho2', 'delta', 'theta_lambda1', 'theta_lambda2')
 unit_estpars <- c('Ri1', 'Ri2', 'I10', 'I20', 'R10', 'R20', 'R120')
 
 # H1, using round 1 CIs:
-res_h1_round1CI <- load_and_format_mega_results(filepath = 'results/round2_flu_h1_round1ci/',
+res_h1_round1CI <- load_and_format_mega_results(filepath = 'results/round2_flu_h1_round1ci_thetalambda1_thetalambda2/',
                                                 shared_estpars = shared_estpars,
                                                 unit_estpars = unit_estpars,
                                                 run_name = 'H1_round1CIs')
 
-# H1, broad:
-res_h1_broad <- load_and_format_mega_results(filepath = 'results/round2_flu_h1_broad/',
-                                             shared_estpars = shared_estpars,
-                                             unit_estpars = unit_estpars,
-                                             run_name = 'H1_broad')
+# # H1, broad:
+# res_h1_broad <- load_and_format_mega_results(filepath = 'results/round2_flu_h1_broad/',
+#                                              shared_estpars = shared_estpars,
+#                                              unit_estpars = unit_estpars,
+#                                              run_name = 'H1_broad')
 
 # B, using round 1 CIs:
-res_b_round1CI <- load_and_format_mega_results(filepath = 'results/round2_flu_b_round1ci/',
+res_b_round1CI <- load_and_format_mega_results(filepath = 'results/round2_flu_b_round1ci_thetalambda1_thetalambda2/',
                                                shared_estpars = shared_estpars,
                                                unit_estpars = unit_estpars,
                                                run_name = 'B_round1CIs')
 
-# B, broad:
-res_b_broad <- load_and_format_mega_results(filepath = 'results/round2_flu_b_broad/',
-                                            shared_estpars = shared_estpars,
-                                            unit_estpars = unit_estpars,
-                                            run_name = 'B_broad')
+# # B, broad:
+# res_b_broad <- load_and_format_mega_results(filepath = 'results/round2_flu_b_broad/',
+#                                             shared_estpars = shared_estpars,
+#                                             unit_estpars = unit_estpars,
+#                                             run_name = 'B_broad')
 
 # Extract results:
-res_LIST <- list(res_h1_round1CI, res_h1_broad, res_b_round1CI, res_b_broad)
-# res_LIST <- list(res_h1_round1CI, res_b_round1CI)
+# res_LIST <- list(res_h1_round1CI, res_h1_broad, res_b_round1CI, res_b_broad)
+res_LIST <- list(res_h1_round1CI, res_b_round1CI)
 pars_top_LIST = pars_top_long_LIST = pars_corr_LIST = vector('list', length = length(res_LIST))
 for (i in 1:length(res_LIST)) {
   pars_top_LIST[[i]] <- res_LIST[[i]][[1]]
@@ -114,13 +114,13 @@ for (i in 1:length(res_LIST)) {
 }
 rm(i)
 
-names(pars_top_LIST) <- c('flu_h1_round1CI', 'flu_h1_broad', 'flu_b_round1CI', 'flu_b_broad')
-names(pars_top_long_LIST) <- c('flu_h1_round1CI', 'flu_h1_broad', 'flu_b_round1CI', 'flu_b_broad')
-names(pars_corr_LIST) <- c('flu_h1_round1CI', 'flu_h1_broad', 'flu_b_round1CI', 'flu_b_broad')
+# names(pars_top_LIST) <- c('flu_h1_round1CI', 'flu_h1_broad', 'flu_b_round1CI', 'flu_b_broad')
+# names(pars_top_long_LIST) <- c('flu_h1_round1CI', 'flu_h1_broad', 'flu_b_round1CI', 'flu_b_broad')
+# names(pars_corr_LIST) <- c('flu_h1_round1CI', 'flu_h1_broad', 'flu_b_round1CI', 'flu_b_broad')
 
-# names(pars_top_LIST) <- c('flu_h1_round1CI', 'flu_b_round1CI')
-# names(pars_top_long_LIST) <- c('flu_h1_round1CI', 'flu_b_round1CI')
-# names(pars_corr_LIST) <- c('flu_h1_round1CI', 'flu_b_round1CI')
+names(pars_top_LIST) <- c('flu_h1_round1CI', 'flu_b_round1CI')
+names(pars_top_long_LIST) <- c('flu_h1_round1CI', 'flu_b_round1CI')
+names(pars_corr_LIST) <- c('flu_h1_round1CI', 'flu_b_round1CI')
 
 # Clean up:
 try(rm(res_LIST, res_h1_round1CI, res_h1_broad, res_b_round1CI, res_b_broad))
@@ -167,7 +167,7 @@ rm(res_r1_noInt, res_r1_int)
 # Visualize results
 
 # Save pairs plots/estimate comparisons as pdf:
-pdf(paste0('results/plots/', date, '_trajectory_matching_round2.pdf'),
+pdf(paste0('results/plots/', date, '_trajectory_matching_round2_thetalambda1_thetalambda2.pdf'),
     width = 18, height = 10)
 
 # Pairs plots:
@@ -181,16 +181,16 @@ res <- bind_rows(pars_top_long_LIST) %>%
   mutate(vir1 = str_sub(method, 1, 2))
 rm(res_r1)
 res$param <- factor(res$param)
-res$param <- factor(res$param, levels = levels(res$param)[c(9:10, 2:4, 6, 5, 7:8, 1, 11)])
+res$param <- factor(res$param, levels = levels(res$param)[c(9:10, 2:4, 6, 5, 7:8, 1, 11:12)])
 
 res_h1 <- res %>% filter(vir1 == 'H1')
 res_b <- res %>% filter(vir1 == 'B_') %>% mutate(vir1 = 'B')
 
 res_h1$method <- factor(res_h1$method)
-res_h1$method <- factor(res_h1$method, levels = levels(res_h1$method)[c(4, 1, 3, 2)])
+res_h1$method <- factor(res_h1$method, levels = levels(res_h1$method)[3:1])
 
 res_b$method <- factor(res_b$method)
-res_b$method <- factor(res_b$method, levels = levels(res_b$method)[c(4, 1, 3, 2)])
+res_b$method <- factor(res_b$method, levels = levels(res_b$method)[3:1])
 
 p1 <- ggplot(data = res_h1, aes(x = year, y = value, fill = method)) + geom_boxplot() +
   facet_wrap(~ param, scales = 'free_y') + theme_classic() + scale_fill_brewer(palette = 'Set1')
@@ -203,7 +203,7 @@ print(p2)
 dev.off()
 
 # Plot correlations between global parameters and Ris/initial conditions:
-pdf(paste0('results/plots/', date, '_trajectory_matching_round2_corr.pdf'),
+pdf(paste0('results/plots/', date, '_trajectory_matching_round2_corr_thetalambda1_thetalambda2.pdf'),
     width = 18, height = 10)
 
 for (i in 1:length(pars_top_LIST)) {
@@ -222,7 +222,7 @@ rm(i)
 dev.off()
 
 # Plot slices:
-pdf(paste0('results/plots/', date, '_trajectory_matching_round2_slices.pdf'),
+pdf(paste0('results/plots/', date, '_trajectory_matching_round2_slices_thetalambda1_thetalambda2.pdf'),
     width = 18, height = 10)
 
 true_estpars <- c(shared_estpars, unit_estpars)
@@ -231,7 +231,7 @@ prof_lik <- FALSE
 for (i in 1:length(pars_top_LIST)) {
   
   # Set estpars:
-  estpars <- names(pars_top_LIST[[i]])[1:39]
+  estpars <- names(pars_top_LIST[[i]])[1:40]
   
   # Set vir1:
   if (str_detect(names(pars_top_LIST)[i], 'h1')) {
@@ -244,10 +244,10 @@ for (i in 1:length(pars_top_LIST)) {
   source('src/functions/setup_global_likelilhood.R')
   
   # Loop through top 5 parameter sets and calculate/plot slices over global params:
-  par(mfrow = c(5, 4), bty = 'l')
+  par(mfrow = c(5, 5), bty = 'l')
   
   for (j in 1:5) {
-    mle <- setNames(object = as.numeric(pars_top_LIST[[i]][j, 1:39]),
+    mle <- setNames(object = as.numeric(pars_top_LIST[[i]][j, 1:40]),
                     nm = estpars)
     # slices <- slice_design(center = mle,
     #                        rho1 = seq(from = 0, to = 1.0, by = 0.05),
@@ -259,11 +259,12 @@ for (i in 1:length(pars_top_LIST)) {
                            rho1 = seq(from = 0.01, to = 0.2, by = 0.01),
                            rho2 = seq(from = 0.01, to = 0.2, by = 0.01),
                            theta_lambda1 = seq(from = 0, to = 0.2, by = 0.01),
+                           theta_lambda2 = seq(from = 0, to = 0.2, by = 0.01),
                            delta = seq(from = 0.01, to = 0.3, by = 0.01)) %>%
       mutate(ll = NA)
     
     for (k in 1:nrow(slices)) {
-      x0 <- slices[k, 1:39]
+      x0 <- slices[k, 1:40]
       x0_trans <- transform_params(x0, po_list[[1]], seasons, estpars, shared_estpars)
       slices$ll[k] <- -1 * calculate_global_loglik(x0_trans)
     }
@@ -287,7 +288,7 @@ rm(i)
 dev.off()
 
 # Plot simulations:
-pdf(paste0('results/plots/', date, '_trajectory_matching_round2_simulations.pdf'),
+pdf(paste0('results/plots/', date, '_trajectory_matching_round2_simulations_thetalambda1_thetalambda2.pdf'),
     width = 18, height = 10)
 
 for (i in 1:length(pars_top_LIST)) {
@@ -318,7 +319,7 @@ for (i in 1:length(pars_top_LIST)) {
     pars_temp <- pars_top_LIST[[i]] %>%
       select(all_of(shared_estpars),
              contains(yr))
-    names(pars_temp)[5:11] <- unit_estpars
+    names(pars_temp)[6:12] <- unit_estpars
     
     # Plot top 5 parameter sets:
     for (k in 1:5) {
