7162c88b899d307bcf45ed268af5552929599543
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2021-11-26 12:22:39 +0100

    Clean up and organize profile likelihood plots

--- src/process_results/analyze_traj_matching_proflik.R
+++ src/process_results/analyze_traj_matching_proflik.R
@@ -41,7 +41,10 @@ load_and_format_proflik_results <- function(filepath, prof_par, shared_estpars) 
            profpar = '...7',
            run = '...8') %>%
     mutate(profpar = as.numeric(profpar),
-           run = as.numeric(run)) %>%
+           run = as.numeric(run),
+           vir1 = if_else(vir1 == 'b', 'B', 'H1'),
+           vir1 = factor(vir1),
+           vir1 = relevel(vir1, ref = 'H1')) %>%
     arrange(vir1, profpar, run)
   expect_true(nrow(res_temp) == length(res_files))
   expect_true(all(is.finite(res_temp$loglik)))
@@ -56,9 +59,9 @@ load_and_format_proflik_results <- function(filepath, prof_par, shared_estpars) 
       mutate(profpar = seq(0.0, 1.0, by = 0.02)[profpar])
   }
   
-  # Rename profpar column:
-  res_temp <- res_temp %>%
-    rename(!!prof_par := 'profpar')
+  # # Rename profpar column:
+  # res_temp <- res_temp %>%
+  #   rename(!!prof_par := 'profpar')
   
   # Return formatted results:
   return(res_temp)
@@ -85,6 +88,7 @@ res_delta <- load_and_format_proflik_results(filepath = 'results/prof_lik_delta/
 
 # Combine all results?:
 res_list <- list(res_thetalambda1, res_thetalambda2, res_delta)
+names(res_list) <- c('theta_lambda1', 'theta_lambda2', 'delta')
 rm(res_thetalambda1, res_thetalambda2, res_delta)
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -105,99 +109,51 @@ ci_cutoff <- lapply(maxloglik, function(ix) {
   ix - 0.5 * qchisq(df = 1, p = 0.99)
 })
 res_list <- lapply(1:length(res_list), function(ix) {
-  res_list[[ix]] %>% mutate(ci = if_else(vir1 == 'b', ci_cutoff[[ix]][1], ci_cutoff[[ix]][2]))
+  res_list[[ix]] %>% mutate(ci = if_else(vir1 == 'B', ci_cutoff[[ix]][2], ci_cutoff[[ix]][1]))
 })
+names(res_list) <- c('theta_lambda1', 'theta_lambda2', 'delta')
+
+# Get top estimate for each value of profpar:
+res_list_top <- lapply(res_list, function(ix) {
+  ix %>%
+    group_by(vir1, profpar) %>%
+    filter(rank(-loglik) == 1) %>%
+    ungroup()
+})
+names(res_list_top) <- c('theta_lambda1', 'theta_lambda2', 'delta')
 
 # Plot profile likelihoods with cutoff for 99% CI:
-
-
-p1a <- ggplot(data = res_list[[1]], aes(x = theta_lambda1, y = loglik)) +
-  geom_point() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y')
-p1b <- ggplot(data = res_list[[2]], aes(x = theta_lambda2, y = loglik)) +
-  geom_point() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y')
-p1c <- ggplot(data = res_list[[3]], aes(x = delta, y = loglik)) +
-  geom_point() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y')
-
-res_list[[1]] %>%
-  group_by(vir1, theta_lambda1) %>%
-  filter(rank(-loglik) < 3) %>%
-  ungroup() %>%
-  ggplot(aes(x = theta_lambda1, y = loglik)) +
-  geom_point() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y') +
-  geom_smooth(method = 'loess', span = 0.25) +
-  geom_hline(color = 'red', aes(yintercept = ci))
-res_list[[2]] %>%
-  group_by(vir1, theta_lambda2) %>%
-  filter(rank(-loglik) < 3) %>%
-  ungroup() %>%
-  ggplot(aes(x = theta_lambda2, y = loglik)) +
-  geom_point() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y') +
-  geom_smooth(method = 'loess', span = 0.25) +
-  geom_hline(color = 'red', aes(yintercept = ci))
-res_list[[3]] %>%
-  group_by(vir1, delta) %>%
-  filter(rank(-loglik) < 3) %>%
-  ungroup() %>%
-  ggplot(aes(x = delta, y = loglik)) +
-  geom_point() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y') +
-  geom_smooth(method = 'loess', span = 0.25) +
-  geom_hline(color = 'red', aes(yintercept = ci))
-
-p2a <- ggplot(data = res_list[[1]], aes(x = theta_lambda1, y = loglik, group = theta_lambda1)) +
-  geom_boxplot() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y')
-p2b<- ggplot(data = res_list[[2]], aes(x = theta_lambda2, y = loglik, group = theta_lambda2)) +
-  geom_boxplot() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y')
-p2c <- ggplot(data = res_list[[3]], aes(x = delta, y = loglik, group = delta)) +
-  geom_boxplot() + theme_classic() + facet_wrap(~ vir1, scales = 'free_y') +
-  scale_x_continuous(trans = 'reciprocal', breaks = 7 / seq(5, 255, by = 10), labels = seq(5, 255, by = 10)) +
-  labs(x = '7 / delta')
-
-
-
-
-
-# Save plots to file:
-pdf('results/plots/prof_lik_tj.pdf', width = 10, height = 4.5)
-print(p1a)
-print(p1b)
-print(p1c)
-print(p2a)
-print(p2b)
-print(p2c)
-dev.off()
-
-
-
-
-
-maxloglik <- res_thetalambda1 %>%
-  group_by(vir1) %>%
-  summarise(loglik = max(loglik)) %>%
-  pull(loglik) %>%
-  unlist()
-
-
-
-
-
-
-
-
+plot_list <- lapply(1:length(res_list_top), function(ix) {
+  ggplot(res_list_top[[ix]], aes(x = profpar, y = loglik)) +
+    geom_point() + theme_classic() +
+    facet_wrap(~ vir1, scales = 'free_y') +
+    geom_smooth(method = 'loess', span = 0.25) +
+    geom_hline(color = 'red', aes(yintercept = ci)) +
+    labs(x = names(res_list_top)[ix], y = 'Log Likelihood')
+})
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 # How do other shared parameter values change as prof_par changes?
 
-res_list[[1]] %>%
-  filter(vir1 == 'h1') %>%
-  select(rho1:theta_lambda2, theta_lambda1) %>%
-  pairs(pch = 20)
-res_list[[1]] %>%
-  filter(vir1 == 'b') %>%
-  select(rho1:theta_lambda2, theta_lambda1) %>%
-  pairs(pch = 20)
+for (i in 1:length(res_list)) {
+  res_list[[i]] %>%
+    filter(vir1 == 'H1') %>%
+    select(all_of(shared_estpars[shared_estpars != names(res_list)[i]]), profpar) %>%
+    pairs(pch = 20)
+  
+  res_list[[i]] %>%
+    filter(vir1 == 'B') %>%
+    select(all_of(shared_estpars[shared_estpars != names(res_list)[i]]), profpar) %>%
+    pairs(pch = 20)
+}
 
+# ---------------------------------------------------------------------------------------------------------------------
 
+# Save plots to file:
+pdf(paste0('results/plots/', date, '_prof_lik_tj.pdf'), width = 10, height = 4.5)
+print(plot_list)
+dev.off()
 
 # ---------------------------------------------------------------------------------------------------------------------
 
