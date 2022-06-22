d62e01b015cca3c97b5527a3e32cd30c20a51c8b
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2022-06-22 07:39:34 +0200

    Use HPDI; check that no simulations go below zero

--- src/bootstrap_03_process_and_CIs.R
+++ src/bootstrap_03_process_and_CIs.R
@@ -5,6 +5,8 @@
 # Load libraries:
 library(tidyverse)
 library(testthat)
+library(rethinking)
+library(gt)
 
 # Get names of all results files:
 file_list_h1 <- list.files(path = 'results/bootstrapping/flu_H1/', full.names = TRUE)
@@ -30,7 +32,7 @@ res_df_h1 <- lapply(res_full_h1, getElement, 'estpars') %>%
   bind_rows() %>%
   bind_cols('loglik' = lapply(res_full_h1, getElement, 'll') %>%
               unlist()) %>%
-  mutate(dataset = str_split(file_list_h1, '_') %>% map(~ .x[6]) %>% unlist()) %>%
+  mutate(dataset = str_split(file_list_h1, '_') %>% purrr::map(~ .x[6]) %>% unlist()) %>%
   bind_cols('conv' = lapply(res_full_h1, getElement, 'message') %>%
               unlist())
 expect_true(nrow(res_df_h1) == length(file_list_h1))
@@ -40,7 +42,7 @@ res_df_b <- lapply(res_full_b, getElement, 'estpars') %>%
   bind_rows() %>%
   bind_cols('loglik' = lapply(res_full_b, getElement, 'll') %>%
               unlist()) %>%
-  mutate(dataset = str_split(file_list_b, '_') %>% map(~ .x[6]) %>% unlist()) %>%
+  mutate(dataset = str_split(file_list_b, '_') %>% purrr::map(~ .x[6]) %>% unlist()) %>%
   bind_cols('conv' = lapply(res_full_b, getElement, 'message') %>%
               unlist())
 expect_true(nrow(res_df_b) == length(file_list_b))
@@ -77,6 +79,66 @@ expect_true(nrow(res_df_b) == 500)
 hist(res_df_h1$loglik, breaks = 50)
 hist(res_df_b$loglik, breaks = 50)
 
+# # Check that no states go below zero for any of the top-fit parameter sets:
+# unit_estpars <- c('Ri1', 'Ri2', 'I10', 'I20', 'R10', 'R20', 'R120')
+# shared_estpars <- res_df_h1 %>% select(!contains(unit_estpars) & !'loglik' & !'dataset') %>% names()
+# true_estpars <- c(shared_estpars, unit_estpars)
+# 
+# prof_lik <- FALSE
+# lag_val <- 0
+# 
+# vir1 <- 'flu_h1'
+# source('src/functions/setup_global_likelilhood.R')
+# 
+# traj_list <- lapply(1:length(seasons), function(ix) {
+#   pars_temp <- res_df_h1 %>%
+#     select(all_of(shared_estpars), contains(seasons[ix]))
+#   names(pars_temp) <- true_estpars
+#   
+#   p_mat <- parmat(coef(po_list[[ix]]), nrep = nrow(pars_temp))
+#   for (param in names(pars_temp)) {
+#     p_mat[param, ] <- pars_temp %>% pull(param)
+#   }
+#   
+#   trajectory(object = po_list[[ix]],
+#              params = p_mat,
+#              format = 'data.frame') %>%
+#     select(!(H1_tot:H2_tot)) %>%
+#     pivot_longer(X_SS:H2,
+#                  names_to = 'state')
+# })
+# 
+# expect_false(any(lapply(traj_list, function(ix) {
+#   any(ix[, 'value'] < 0)
+# }) %>%
+#   unlist()))
+# 
+# vir1 <- 'flu_b'
+# source('src/functions/setup_global_likelilhood.R')
+# 
+# traj_list <- lapply(1:length(seasons), function(ix) {
+#   pars_temp <- res_df_h1 %>%
+#     select(all_of(shared_estpars), contains(seasons[ix]))
+#   names(pars_temp) <- true_estpars
+#   
+#   p_mat <- parmat(coef(po_list[[ix]]), nrep = nrow(pars_temp))
+#   for (param in names(pars_temp)) {
+#     p_mat[param, ] <- pars_temp %>% pull(param)
+#   }
+#   
+#   trajectory(object = po_list[[ix]],
+#              params = p_mat,
+#              format = 'data.frame') %>%
+#     select(!(H1_tot:H2_tot)) %>%
+#     pivot_longer(X_SS:H2,
+#                  names_to = 'state')
+# })
+# 
+# expect_false(any(lapply(traj_list, function(ix) {
+#   any(ix[, 'value'] < 0)
+# }) %>%
+#   unlist()))
+
 # Calculate 99% confidence intervals for each parameter:
 res_df_h1_long <- res_df_h1 %>%
   select(-c(loglik, dataset)) %>%
@@ -85,9 +147,14 @@ res_df_h1_long <- res_df_h1 %>%
 
 ci_res <- res_df_h1_long %>%
   group_by(parameter) %>%
-  summarise(lower = quantile(value, p = 0.005),
-            upper = quantile(value, p = 0.995)) %>%
+  summarise(lower = HPDI(value, p = 0.99)[1],
+            upper = HPDI(value, p = 0.99)[2]) %>%
   mutate(vir1 = 'flu_h1')
+# ci_res <- res_df_h1_long %>%
+#   group_by(parameter) %>%
+#   summarise(lower = quantile(value, p = 0.005),
+#             upper = quantile(value, p = 0.995)) %>%
+#   mutate(vir1 = 'flu_h1')
 
 res_df_b_long <- res_df_b %>%
   select(-c(loglik, dataset)) %>%
@@ -97,12 +164,52 @@ res_df_b_long <- res_df_b %>%
 ci_res <- ci_res %>%
   bind_rows(res_df_b_long %>%
               group_by(parameter) %>%
-              summarise(lower = quantile(value, p = 0.005),
-                        upper = quantile(value, p = 0.995)) %>%
+              summarise(lower = HPDI(value, p = 0.99)[1],
+                        upper = HPDI(value, p = 0.99)[2]) %>%
               mutate(vir1 = 'flu_b'))
+# ci_res <- ci_res %>%
+#   bind_rows(res_df_b_long %>%
+#               group_by(parameter) %>%
+#               summarise(lower = quantile(value, p = 0.005),
+#                         upper = quantile(value, p = 0.995)) %>%
+#               mutate(vir1 = 'flu_b'))
 
 # Write results to file:
-write_csv(ci_res, file = 'results/99CI_from_boostrapping.csv')
+write_csv(ci_res, file = 'results/99CI_from_boostrapping_HPDI.csv')
+
+# Read in MLEs and add to data frame:
+mle <- read_csv('results/mle.csv')
+mle$vir1 = c('flu_h1', 'flu_b')
+mle <- mle %>%
+  pivot_longer(cols = !vir1, names_to = 'parameter', values_to = 'mle')
+
+ci_res <- ci_res %>%
+  left_join(mle, by = c('vir1', 'parameter')) %>%
+  select(parameter, mle, lower:vir1)
+
+# Generate tables of results:
+table_h1 <- ci_res %>%
+  filter(vir1 == 'flu_h1') %>%
+  select(-vir1) %>%
+  gt() %>%
+  tab_header(title = 'Best-Fit Parameter Values (Flu H1)') %>%
+  fmt_number(columns = c(mle, lower, upper), decimals = 3, suffixing = TRUE)
+table_b <- ci_res %>%
+  filter(vir1 == 'flu_b') %>%
+  select(-vir1) %>%
+  gt() %>%
+  tab_header(title = 'Best-Fit Parameter Values (Flu B)') %>%
+  fmt_number(columns = c(mle, lower, upper), decimals = 3, suffixing = TRUE)
+
+print(table_h1)
+print(table_b)
+
+gtsave(table_h1, filename = 'results/plots/table_CIs_h1.html')
+gtsave(table_b, filename = 'results/plots/table_CIs_b.html')
+
+# Check whether MLEs fall within CIs:
+ci_res %>% filter(mle <= lower) # s18-19_R10 (B)
+ci_res %>% filter(mle >= upper) # s15-16_R20 (B), s18-19_R120 (B)
 
 # Plot range of fit values for each parameter:
 res_df_h1_NOSHARED <- res_df_h1_long %>%
