3a6f1334e30d1699857660580f4dafeb411dc2bd
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2022-09-13 11:39:35 +0200

    Update code to explore interaction effect

--- src/checks/explore_interaction_impact.R
+++ src/checks/explore_interaction_impact.R
@@ -19,67 +19,21 @@ prof_lik <- FALSE
 
 # Functions
 
-# Function to read in and format results:
-load_and_format_mega_results <- function(filepath, true_estpars, run_name) {
-  
-  # # Compile estpars:
-  # true_estpars <- c(shared_estpars, unit_estpars)
-  
-  # Get list of results files:
-  res_files <- list.files(path = filepath, full.names = TRUE)
-  
-  # Read in results:
-  res_full = list()
-  for (i in seq_along(res_files)) {
-    res_full[[i]] <- read_rds(res_files[[i]])
-  }
-  
-  # Get parameter estimates and log-likelihoods:
-  pars_df <- lapply(res_full, getElement, 'estpars') %>%
-    bind_rows() %>%
-    bind_cols('loglik' = lapply(res_full, getElement, 'll') %>%
-                unlist()) %>%
-    bind_cols('message' = lapply(res_full, getElement, 'message') %>%
-                unlist())
-  expect_true(nrow(pars_df) == length(res_files))
-  expect_true(all(is.finite(pars_df$loglik)))
-  
-  # Keep only top results:
-  pars_df <- pars_df %>%
-    arrange(desc(loglik))
-  
-  df_use <- pars_df %>% select(-c(loglik, message)) %>% names() %>% length()
-  expect_equal(df_use, 47)
-  
-  no_best <- nrow(subset(pars_df, 2 * (max(loglik) - loglik) <= qchisq(p = 0.95, df = df_use)))
-  no_best <- max(no_best, 50)
-  
-  pars_top <- pars_df[1:no_best, ]
-  
-  # Return formatted results:
-  return(pars_top)
-  
-}
-
 # Function to generate slices/grids and calculate log-likelihoods:
-calculate_slices_and_grid <- function(pars_top, vir1, shared_estpars) {
+calculate_slices_and_grid <- function(mle) {
   
   # Set estpars:
-  estpars <- names(pars_top %>%
-                     select(-loglik))
+  estpars <- names(mle)
   
   # Read in pomp models:
   source('src/functions/setup_global_likelilhood.R')
   
-  # Get MLE:
-  mle <- setNames(object = as.numeric(pars_top[1, 1:length(estpars)]),
-                  nm = estpars)
-  
   # Get slices and calculate ll:
-  slices <- slice_design(center = mle,
+  slices <- slice_design(center = unlist(mle),
                          theta_lambda1 = seq(from = 0, to = 1.0, by = 0.025),
                          theta_lambda2 = seq(from = 0, to = 1.0, by = 0.025),
-                         delta = seq(from = 0.01, to = 1.4, by = 0.025)) %>%
+                         delta1 = seq(from = 0.01, to = 1.4, by = 0.025),
+                         d2 = seq(from = 0.5, to = 5.0, by = 0.5)) %>%
     mutate(ll = NA)
   
   for (ix in 1:nrow(slices)) {
@@ -90,42 +44,61 @@ calculate_slices_and_grid <- function(pars_top, vir1, shared_estpars) {
   
   # Get grid and calculate ll:
   theta_lambda1_vals <- seq(from = 0, to = 1.0, by = 0.025)
-  delta_vals <- seq(from = 0.01, to = 1.4, by = 0.025)
+  delta1_vals <- seq(from = 0.01, to = 1.4, by = 0.025)
   
-  grid <- expand_grid(theta_lambda1_vals, delta_vals) %>%
+  grid <- expand_grid(theta_lambda1_vals, delta1_vals) %>%
     rename('theta_lambda1' = 'theta_lambda1_vals',
-           'delta' = 'delta_vals') %>%
+           'delta1' = 'delta1_vals') %>%
     mutate(ll = NA)
   
   x0 <- mle
   for (ix in 1:nrow(grid)) {
-    x0[c('theta_lambda1', 'delta')] <- grid[ix, ]
+    x0[c('theta_lambda1', 'delta1')] <- grid[ix, c('theta_lambda1', 'delta1')]
     x0_trans <- transform_params(x0, po_list[[1]], seasons, estpars, shared_estpars)
     grid$ll[ix] <- -1 * calculate_global_loglik(x0_trans)
   }
   
+  theta_lambda2_vals <- seq(from = 0, to = 1.0, by = 0.025)
+  d2_vals <- seq(from = 0.5, to = 5.0, by = 0.5)
+  
+  grid2 <- expand_grid(theta_lambda2_vals, d2_vals) %>%
+    rename('theta_lambda2' = 'theta_lambda2_vals',
+           'd2' = 'd2_vals') %>%
+    mutate(ll = NA)
+  
+  x0 <- mle
+  for (ix in 1:nrow(grid2)) {
+    x0[c('theta_lambda2', 'd2')] <- grid2[ix, c('theta_lambda2', 'd2')]
+    x0_trans <- transform_params(x0, po_list[[1]], seasons, estpars, shared_estpars)
+    grid2$ll[ix] <- -1 * calculate_global_loglik(x0_trans)
+  }
+  
   # Return data frames:
-  return(list(slices, grid))
+  return(list(slices, grid, grid2))
   
 }
 
 # Function to simulate for each season and parameter combination:
-simulate_grid <- function(pars_top, vir1, shared_estpars, unit_estpars, param1, param2, int_val_ranges, n_sim = 5) {
+simulate_grid <- function(mle, param1, int_val_ranges, n_sim = 5) {
   
   # Read in pomp models:
   source('src/functions/setup_global_likelilhood.R')
   
   # Get interaction parameter values:
-  if (param1 == 'delta') {
-    param1_vals <- int_val_ranges[[2]]
-  } else {
+  if (param1 == 'theta_lambda1') {
+    
+    param2 <- 'delta1'
+    
     param1_vals <- int_val_ranges[[1]]
-  }
-  
-  if (param2 == 'delta') {
     param2_vals <- int_val_ranges[[2]]
-  } else {
-    param2_vals <- int_val_ranges[[1]]
+    
+  } else if (param1 == 'theta_lambda2') {
+    
+    param2 <- 'd2'
+    
+    param1_vals <- int_val_ranges[[1]]
+    param2_vals <- int_val_ranges[[3]]
+    
   }
   
   # Get grid of interaction parameters:
@@ -135,17 +108,18 @@ simulate_grid <- function(pars_top, vir1, shared_estpars, unit_estpars, param1, 
   
   # Get parameters for simulations:
   p_mat_list <- lapply(1:length(seasons), function(ix) {
-    pars_temp <- pars_top %>%
-      # arrange(desc(loglik)) %>%
+    
+    pars_temp <- mle %>%
       select(all_of(shared_estpars), contains(seasons[ix]))
     names(pars_temp) <- c(shared_estpars, unit_estpars)
     
-    coef(po_list[[ix]], names(pars_temp)) <- pars_temp[1, ]
+    coef(po_list[[ix]], names(pars_temp)) <- pars_temp
     p_mat <- parmat(coef(po_list[[ix]]), nrep = nrow(grid_vals))
     p_mat[param1, ] <- unlist(grid_vals[, param1])
     p_mat[param2, ] <- unlist(grid_vals[, param2])
     
     p_mat
+    
   })
   
   # Simulate for each season and parameter set:
@@ -176,55 +150,28 @@ simulate_grid <- function(pars_top, vir1, shared_estpars, unit_estpars, param1, 
 # ---------------------------------------------------------------------------------------------------------------------
 
 # Set shared and unit parameters:
-shared_estpars <- c('rho1', 'rho2', 'delta', 'theta_lambda1', 'theta_lambda2')
+shared_estpars <- c('rho1', 'rho2', 'theta_lambda1', 'theta_lambda2', 'delta1', 'd2',
+                    'alpha', 'phi', 'eta_temp1', 'eta_temp2', 'eta_ah1', 'eta_ah2')
 unit_estpars <- c('Ri1', 'Ri2', 'I10', 'I20', 'R10', 'R20', 'R120')
 true_estpars <- c(shared_estpars, unit_estpars)
 
-# Read in results (round 2):
-res_h1 <- load_and_format_mega_results(filepath = 'results/round2_flu_h1_round1ci/',
-                                       true_estpars = true_estpars,
-                                       run_name = 'flu_H1') %>%
-  mutate(theta_lambda2 = 1.0) %>%
-  select(rho1:theta_lambda1, theta_lambda2, `s13-14_Ri1`:loglik)
-res_b <- load_and_format_mega_results(filepath = 'results/round2_flu_b_round1ci/',
-                                      true_estpars = true_estpars,
-                                      run_name = 'flu_B') %>%
-  mutate(theta_lambda2 = 1.0) %>%
-  select(rho1:theta_lambda1, theta_lambda2, `s13-14_Ri1`:loglik)
-res_h1_BOTH <- load_and_format_mega_results(filepath = 'results/round2_flu_h1_round1ci_thetalambda1_thetalambda2/',
-                                            true_estpars = true_estpars,
-                                            run_name = 'flu_H1_BOTH')
-
-res_list <- list(res_h1, res_b, res_h1_BOTH)
-names(res_list) <- c('H1', 'B', 'H1_both')
-rm(res_h1, res_b, res_h1_BOTH)
+# Read in MLEs:
+mle_h1 <- read_rds('results/MLEs_flu_h1.rds')[1, ]
+mle_b <- read_rds('results/MLEs_flu_b.rds')[1, ]
 
 # ---------------------------------------------------------------------------------------------------------------------
 
 # Slice/grid likelihoods
-# Slices: theta_lambda1, theta_lambda2, delta
-# Grid: theta_lambda1, delta
 
-# Loop through results and get slices:
-slice_list = grid_list = vector('list', length = length(res_list))
-for (i in 1:length(res_list)) {
-  
-  # Get vir1:
-  if (str_detect(names(res_list)[i], 'H1')) {
-    vir1 <- 'flu_h1'
-  } else {
-    vir1 <- 'flu_b'
-  }
-  
-  # Calculate ll over slices:
-  res_temp <- calculate_slices_and_grid(res_list[[i]], vir1, shared_estpars)
-  
-  # Store slices:
-  slice_list[[i]] <- res_temp[[1]]
-  grid_list[[i]] <- res_temp[[2]]
-  
-}
-rm(i, vir1, res_temp, dat_pomp, hk_dat, obj_fun_list, po_list, seasons, yr, yr_index)
+# Get slices/grids:
+vir1 <- 'flu_h1'
+slice_grid_ll_h1 <- calculate_slices_and_grid(mle_h1)
+
+vir1 <- 'flu_b'
+slice_grid_ll_b <- calculate_slices_and_grid(mle_b)
+
+res_list <- list(slice_grid_ll_h1, slice_grid_ll_b)
+rm(slice_grid_ll_h1, slice_grid_ll_b)
 
 # Save plots to file?:
 if (save_plots) {
@@ -232,32 +179,45 @@ if (save_plots) {
 }
 
 # Plot slices:
-par(mfrow = c(3, 3))
+par(mfrow = c(2, 4))
 for (i in 1:length(res_list)) {
-  slice_temp <- slice_list[[i]]
   
-  for (par in c('theta_lambda1', 'theta_lambda2', 'delta')) {
+  slice_temp <- res_list[[i]][[1]]
+  
+  for (par in c('theta_lambda1', 'theta_lambda2', 'delta1', 'd2')) {
     slices_cur <- filter(slice_temp, slice == par)
     plot(slices_cur[[par]], slices_cur$ll, type = 'l',
          xlab = par, ylab = 'Log-Likelihood',
          main = names(res_list)[i])
   }
+  
 }
 rm(i, slice_temp, par, slices_cur)
 
 # Plot grids:
 for (i in 1:length(res_list)) {
-  grid_temp <- grid_list[[i]]
   
-  p_temp <- ggplot(data = grid_temp, aes(x = theta_lambda1, y = delta, fill = ll)) +
+  grid_temp1 <- res_list[[i]][[2]]
+  
+  p_temp1 <- ggplot(data = grid_temp1, aes(x = theta_lambda1, y = delta1, fill = ll)) +
     geom_tile() + theme_classic() +
     scale_fill_viridis() +
     scale_x_continuous(n.breaks = 10, expand = c(0, 0)) +
     scale_y_continuous(n.breaks = 20, expand = c(0, 0)) +
     labs(fill = 'LL')
-  print(p_temp)
+  
+  grid_temp2 <- res_list[[i]][[3]]
+  p_temp2 <- ggplot(data = grid_temp2, aes(x = theta_lambda2, y = d2, fill = ll)) +
+    geom_tile() + theme_classic() +
+    scale_fill_viridis() +
+    scale_x_continuous(n.breaks = 10, expand = c(0, 0)) +
+    scale_y_continuous(n.breaks = 20, expand = c(0, 0)) +
+    labs(fill = 'LL')
+  
+  grid.arrange(p_temp1, p_temp2, nrow = 1)
+  
 }
-rm(i, grid_temp, p_temp)
+rm(i, grid_temp1, grid_temp2, p_temp1, p_temp2)
 
 if (save_plots) {
   dev.off()
@@ -269,49 +229,39 @@ if (save_plots) {
 
 # Set interaction parameter values:
 int_vals <- seq(0, 1, by = 0.1)
-delta_vals <- c(7/5, 7/14, 7/30, 7/60, 7/200)
-int_vals_all <- list(int_vals, delta_vals)
-rm(int_vals, delta_vals)
+delta1_vals <- c(7/5, 7/14, 7/30, 7/60, 7/200)
+d2_vals <- c(0.5, 1.0, 1.5, 2.0, 5.0)
+int_vals_all <- list(int_vals, delta1_vals, d2_vals)
+rm(int_vals, delta1_vals, d2_vals)
 
-# Loop through results:
-sim_list <- vector('list', length = length(res_list))
-for (i in 1:length(res_list)) {
-  
-  # Get vir1:
-  if (str_detect(names(res_list)[i], 'H1')) {
-    vir1 <- 'flu_h1'
-  } else {
-    vir1 <- 'flu_b'
-  }
-  
-  # Run simulations across interaction parameter values:
-  sim_temp <- simulate_grid(res_list[[i]], vir1, shared_estpars, unit_estpars, 'theta_lambda1', 'delta', int_vals_all)
-  
-  # Store simulations:
-  sim_list[[i]] <- sim_temp
-  
-}
-rm(i, int_vals_all, vir1, sim_temp, dat_pomp, hk_dat, obj_fun_list, po_list, seasons, yr, yr_index)
+# Run simulations across interaction parameter values:
+vir1 <- 'flu_h1'
+sim_grid_h1_1 <- simulate_grid(mle_h1, 'theta_lambda1', int_vals_all)
+sim_grid_h1_2 <- simulate_grid(mle_h1, 'theta_lambda2', int_vals_all)
+
+vir1 <- 'flu_b'
+sim_grid_b_1 <- simulate_grid(mle_b, 'theta_lambda1', int_vals_all)
+sim_grid_b_2 <- simulate_grid(mle_b, 'theta_lambda2', int_vals_all)
+
+res_list <- list(sim_grid_h1_1, sim_grid_h1_2, sim_grid_b_1, sim_grid_b_2)
+rm(sim_grid_h1_1, sim_grid_h1_2, sim_grid_b_1, sim_grid_b_2)
 
 # Generate plots of simulations:
 plot_list <- vector('list', length = length(res_list))
-seasons_all <- c('s13-14', 's14-15', 's15-16', 's16-17', 's17-18', 's18-19')
 for (i in 1:length(res_list)) {
-  sim_temp <- sim_list[[i]]
-  sim_temp <- sim_temp %>%
-    mutate(param2_val = 7 / param2_val)
   
-  seasons <- res_list[[i]] %>%
-    select(contains(seasons_all)) %>%
-    pivot_longer(cols = everything(),
-                 names_to = 'season') %>%
-    mutate(season = str_sub(season, 1, 6)) %>%
-    pull(season) %>%
-    unique()
+  sim_temp <- res_list[[i]]
+  if (i %in% c(1, 3)) {
+    sim_temp <- sim_temp %>%
+      mutate(param2_val = 7 / param2_val)
+  }
+  
+  seasons <- unique(sim_temp$season)
   
   plot_list_temp <- vector('list', length = length(seasons))
   
   for (j in 1:length(seasons)) {
+    
     sim_temp_yr <- sim_temp %>%
       filter(season == seasons[j])
     
@@ -328,28 +278,16 @@ for (i in 1:length(res_list)) {
       scale_color_brewer(palette = 'Set1') +
       labs(x = 'Time (Weeks)', y = 'Postive Tests', col = 'Vir.', title = seasons[j])
     
-    # p_obs_alt1 <- ggplot(data = sim_temp_yr %>%
-    #                        filter(output %in% c('n_P1', 'n_P2')),
-    #                      aes(x = time, y = val, col = param1_val, lty = output, group = paste(output, id.sim, param1_val))) +
-    #   geom_line() + theme_classic() + facet_wrap(~ param2_val) +
-    #   scale_color_viridis() +
-    #   labs(x = 'Time (Weeks)', y = 'Postive Tests', col = 'theta_lambda1', title = seasons[j])
-    # p_obs_alt2 <- ggplot(data = sim_temp_yr %>%
-    #                        filter(output %in% c('n_P1', 'n_P2')),
-    #                      aes(x = time, y = val, col = param2_val, lty = output, group = paste(output, id.sim, param2_val))) +
-    #   geom_line() + theme_classic() + facet_wrap(~ param1_val) +
-    #   scale_color_viridis() +
-    #   labs(x = 'Time (Weeks)', y = 'Postive Tests', col = 'delta', title = seasons[j])
-    # print(p_obs_alt1)
-    # print(p_obs_alt2)
-    
     plot_list_temp[[j]] <- list(p_cases, p_obs)
+    
   }
+  
   rm(j, sim_temp_yr, p_cases, p_obs)
   
   plot_list[[i]] <- plot_list_temp
+  
 }
-rm(i, sim_temp, seasons_all, seasons, plot_list_temp)
+rm(i, sim_temp, seasons, plot_list_temp)
 
 # Save plots to file:
 if (save_plots) {
