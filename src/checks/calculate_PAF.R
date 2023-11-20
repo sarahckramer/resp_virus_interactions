80cc8b642788fd74da490cf5b10a911ccfe08865
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2023-11-20 12:44:59 +0100

    Update code to calculate ARs and PAFs

--- src/checks/calculate_PAF.R
+++ src/checks/calculate_PAF.R
@@ -22,14 +22,13 @@ lag_val <- 0
 # Generate synthetic data at the MLE, with and without interaction
 
 # Read in MLEs:
-mle_h1 <- read_rds('results/MLEs_flu_h1.rds')
-mle_b <- read_rds('results/MLEs_flu_b.rds')
+mle <- read_rds('results/MLEs_flu_h1_plus_b.rds')
 
-# Get synthetic data (H1N1/RSV):
-vir1 <- 'flu_h1'
+# Get synthetic data:
+vir1 <- 'flu_h1_plus_b'
 source('src/functions/setup_global_likelilhood.R')
 
-traj_list_H1N1 <- vector('list', length = length(seasons))
+traj_list <- vector('list', length = length(seasons))
 for (i in 1:length(seasons)) {
   
   # Get season:
@@ -39,7 +38,7 @@ for (i in 1:length(seasons)) {
   resp_mod <- po_list[[i]]
   
   # Get best-fit model parameters:
-  pars_temp <- mle_h1[1, ] %>%
+  pars_temp <- mle[1, ] %>%
     select(all_of(shared_estpars),
            contains(yr))
   names(pars_temp)[(length(names(pars_temp)) - 6):length(names(pars_temp))] <- unit_estpars
@@ -101,101 +100,13 @@ for (i in 1:length(seasons)) {
   #   select(time:H2, mean_obs1:mean_obs2)
   
   # Store:
-  traj_list_H1N1[[i]] <- traj_temp
-  
-}
-
-# Get synthetic data (B/RSV):
-vir1 <- 'flu_b'
-source('src/functions/setup_global_likelilhood.R')
-
-traj_list_B <- vector('list', length = length(seasons))
-for (i in 1:length(seasons)) {
-  
-  # Get season:
-  yr <- seasons[i]
-  
-  # Get pomp object:
-  resp_mod <- po_list[[i]]
-  
-  # Get best-fit model parameters:
-  pars_temp <- mle_b[1, ] %>%
-    select(all_of(shared_estpars),
-           contains(yr))
-  names(pars_temp)[(length(names(pars_temp)) - 6):length(names(pars_temp))] <- unit_estpars
-  
-  coef(resp_mod, true_estpars) <- pars_temp
-  
-  # Generate matrix of model parameters for fitting:
-  param_mat_temp <- parmat(coef(resp_mod), nrep = 5)
-  
-  # Remove impact of flu from some parameter sets:
-  param_mat_temp['theta_lambda1', 2] <- 1.0
-  param_mat_temp['I10', 3] <- 0
-  
-  # Remove impact of RSV from some parameter sets:
-  param_mat_temp['theta_lambda2', 4] <- 1.0
-  param_mat_temp['I20', 5] <- 0
-  
-  # Simulate using deterministic model:
-  traj_temp <- trajectory(resp_mod, params = param_mat_temp, format = 'data.frame') %>%
-    mutate(season = yr) %>%
-    select(time:season, H1:H2)
-  
-  # Check that removal of interaction works as expected:
-  expect_true(all.equal(traj_temp %>% filter(.id == 2) %>% pull(H2), traj_temp %>% filter(.id == 3) %>% pull(H2)))
-  expect_true(all.equal(traj_temp %>% filter(.id == 4) %>% pull(H1), traj_temp %>% filter(.id == 5) %>% pull(H1)))
-  
-  # Remove unneeded simulations:
-  traj_temp <- traj_temp %>%
-    filter(.id %in% c(1, 3, 5))
-  
-  # # Also look at how # of observed cases changes:
-  # rho1 <- as.numeric(pars_temp['rho1'])
-  # rho2 <- as.numeric(pars_temp['rho2'])
-  # alpha <- as.numeric(pars_temp['alpha'])
-  # phi <- as.numeric(pars_temp['phi'])
-  # 
-  # dat_temp <- hk_dat$b_rsv
-  # traj_temp <- traj_temp %>%
-  #   left_join(dat_temp, by = c('time', 'season')) %>%
-  #   rename('i_ILI' = 'GOPC') %>%
-  #   mutate(i_ILI = i_ILI / 1000) %>%
-  #   select(time:H2, n_T, i_ILI)
-  # 
-  # rho1_w <- rho1 * (1.0 + alpha * cos(((2 * pi) / 52.25) * (traj_temp$time - phi))) * traj_temp$H1 / traj_temp$i_ILI
-  # rho2_w <- rho2 * (1.0 + alpha * cos(((2 * pi) / 52.25) * (traj_temp$time - phi))) * traj_temp$H2 / traj_temp$i_ILI
-  # 
-  # rho1_w[rho1_w > 1.0 & !is.na(rho1_w)] <- 1.0
-  # rho2_w[rho2_w > 1.0 & !is.na(rho2_w)] <- 1.0
-  # 
-  # expect_equal(nrow(traj_temp), length(rho1_w))
-  # expect_equal(nrow(traj_temp), length(rho2_w))
-  # 
-  # traj_temp$rho1_w <- rho1_w
-  # traj_temp$rho2_w <- rho2_w
-  # 
-  # traj_temp <- traj_temp %>%
-  #   mutate(mean_obs1 = rho1_w * n_T,
-  #          mean_obs2 = rho2_w * n_T) %>%
-  #   select(time:H2, mean_obs1:mean_obs2)
-  
-  # Store:
-  traj_list_B[[i]] <- traj_temp
+  traj_list[[i]] <- traj_temp
   
 }
 
 # Compile:
-res_H1N1 <- bind_rows(traj_list_H1N1) %>%
-  mutate(virus_pair = 'Influenza H1N1 / RSV') %>%
+res <- bind_rows(traj_list) %>%
   as_tibble()
-
-res_B <- bind_rows(traj_list_B) %>%
-  mutate(virus_pair = 'Influenza B / RSV') %>%
-  as_tibble()
-
-res <- bind_rows(res_H1N1, res_B)
-rm(res_H1N1, res_B)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -203,83 +114,39 @@ rm(res_H1N1, res_B)
 
 # Calculate PAF (or negative of PAF, i.e., % increase in AR expected if interaction were not present):
 res_ars <- res %>%
-  group_by(virus_pair, season, .id) %>%
+  group_by(season, .id) %>%
   summarise(attack_rate_H1 = sum(H1),
-            attack_rate_H2 = sum(H2))#,
-            # attack_rate_obs1 = sum(mean_obs1, na.rm = TRUE),
-            # attack_rate_obs2 = sum(mean_obs2, na.rm = TRUE))
+            attack_rate_H2 = sum(H2))
 
-paf_h1_fluOnRSV = paf_h1_RSVonFlu = paf_b_fluOnRSV = c()
-paf_h1_obs_fluOnRSV = paf_h1_obs_RSVonFlu = paf_b_obs_fluOnRSV = c()
+paf_fluOnRSV = paf_RSVonFlu = c()
 for (yr in unique(res_ars$season)) {
   
-  res_temp_H1N1 <- res_ars %>%
-    filter(season == yr & virus_pair == 'Influenza H1N1 / RSV')
-  res_temp_B <- res_ars %>%
-    filter(season == yr & virus_pair == 'Influenza B / RSV')
+  res_temp <- res_ars %>%
+    filter(season == yr)
   
-  if (nrow(res_temp_H1N1) > 0) {
+  if (nrow(res_temp) > 0) {
     
     # Impact of flu on RSV:
-    ar_pop <- res_temp_H1N1$attack_rate_H2[res_temp_H1N1$.id == 1]
-    ar_unexposed <- res_temp_H1N1$attack_rate_H2[res_temp_H1N1$.id == 3]
+    ar_pop <- res_temp$attack_rate_H2[res_temp$.id == 1]
+    ar_unexposed <- res_temp$attack_rate_H2[res_temp$.id == 3]
     
-    # paf_h1_fluOnRSV <- c(paf_h1_fluOnRSV, (ar_pop - ar_unexposed) / ar_pop) # negative
-    paf_h1_fluOnRSV <- c(paf_h1_fluOnRSV, (ar_unexposed - ar_pop) / ar_pop)
-    
-    # ar_pop <- res_temp_H1N1$attack_rate_obs2[res_temp_H1N1$.id == 1]
-    # ar_unexposed <- res_temp_H1N1$attack_rate_obs2[res_temp_H1N1$.id == 3]
-    # 
-    # paf_h1_obs_fluOnRSV <- c(paf_h1_obs_fluOnRSV, (ar_unexposed - ar_pop) / ar_pop)
+    # paf_fluOnRSV <- c(paf_fluOnRSV, (ar_pop - ar_unexposed) / ar_pop) # negative
+    paf_fluOnRSV <- c(paf_fluOnRSV, (ar_unexposed - ar_pop) / ar_pop)
     
     # Impact of RSV on flu:
-    ar_pop <- res_temp_H1N1$attack_rate_H1[res_temp_H1N1$.id == 1]
-    ar_unexposed <- res_temp_H1N1$attack_rate_H1[res_temp_H1N1$.id == 5]
+    ar_pop <- res_temp$attack_rate_H1[res_temp$.id == 1]
+    ar_unexposed <- res_temp$attack_rate_H1[res_temp$.id == 5]
     
-    paf_h1_RSVonFlu <- c(paf_h1_RSVonFlu, (ar_unexposed - ar_pop) / ar_pop)
-    
-    # ar_pop <- res_temp_H1N1$attack_rate_obs1[res_temp_H1N1$.id == 1]
-    # ar_unexposed <- res_temp_H1N1$attack_rate_obs1[res_temp_H1N1$.id == 5]
-    # 
-    # paf_h1_obs_RSVonFlu <- c(paf_h1_obs_RSVonFlu, (ar_unexposed - ar_pop) / ar_pop)
-    
-  }
-  
-  if (nrow(res_temp_B) > 0) {
-    
-    # Impact of flu on RSV:
-    ar_pop <- res_temp_B$attack_rate_H2[res_temp_H1N1$.id == 1]
-    ar_unexposed <- res_temp_B$attack_rate_H2[res_temp_H1N1$.id == 3]
-    
-    paf_b_fluOnRSV <- c(paf_b_fluOnRSV, (ar_unexposed - ar_pop) / ar_pop)
-    
-    # ar_pop <- res_temp_B$attack_rate_obs2[res_temp_H1N1$.id == 1]
-    # ar_unexposed <- res_temp_B$attack_rate_obs2[res_temp_H1N1$.id == 3]
-    # 
-    # paf_b_obs_fluOnRSV <- c(paf_b_obs_fluOnRSV, (ar_unexposed - ar_pop) / ar_pop)
+    paf_RSVonFlu <- c(paf_RSVonFlu, (ar_unexposed - ar_pop) / ar_pop)
     
   }
   
 }
 
-print(summary(paf_h1_fluOnRSV))
-print(summary(paf_b_fluOnRSV))
-
-print(summary(paf_h1_RSVonFlu))
-
-# print(summary(paf_h1_obs_fluOnRSV))
-# print(summary(paf_b_obs_fluOnRSV))
-# 
-# print(summary(paf_h1_obs_RSVonFlu))
+print(summary(paf_fluOnRSV))
+print(summary(paf_RSVonFlu))
 
 # Plot how interaction influences outbreak dynamics:
-res <- res %>%
-  mutate(virus_pair = factor(virus_pair, levels = c('Influenza H1N1 / RSV', 'Influenza B / RSV')))
-# res <- res %>%
-#   mutate(Week = time + 45) %>%
-#   mutate(Week = if_else(Week > 52 & season != 's16-17', Week - 52, Week),
-#          Week = if_else(Week > 53 & season == 's16-17', Week - 53, Week))
-
 p1 <- ggplot() +
   geom_line(data = res %>%
               filter(.id %in% c(1, 3)) %>%
@@ -288,7 +155,7 @@ p1 <- ggplot() +
   geom_line(data = res %>%
               filter(.id == 1),
             aes(x = time, y = H1), lty = 2) +
-  facet_grid(virus_pair ~ season, scales = 'free_x') +
+  facet_wrap(~ season, scales = 'free_x', nrow = 1) +
   theme_classic() +
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 12),
@@ -304,15 +171,13 @@ p1 <- ggplot() +
 
 p2 <- ggplot() +
   geom_line(data = res %>%
-              filter(virus_pair == 'Influenza H1N1 / RSV') %>%
               filter(.id %in% c(1, 5)) %>%
               mutate(.id = if_else(.id == 1, 'Interaction', 'No Interaction')),
             aes(x = time, y = H1, col = .id)) +
   geom_line(data = res %>%
-              filter(virus_pair == 'Influenza H1N1 / RSV') %>%
               filter(.id == 1),
             aes(x = time, y = H2), lty = 2) +
-  facet_grid(virus_pair ~ season, scales = 'free_x') +
+  facet_wrap(~ season, scales = 'free_x', nrow = 1) +
   theme_classic() +
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 12),
@@ -326,5 +191,5 @@ p2 <- ggplot() +
   scale_color_manual(values = c('#de2d26', '#fc9272')) +
   labs(x = 'Time (Weeks)', y = 'Influenza Incidence', color = '', tag = 'B')
 
-fig <- arrangeGrob(p1, p2, ncol = 1, heights = c(1.6, 1))
+fig <- arrangeGrob(p1, p2, ncol = 1, heights = c(1, 1))
 plot(fig)
