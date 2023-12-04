8d3dfdfdf4e473ee80efb28dff03f9c0b0bff178
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2023-12-04 09:26:31 +0100

    Add no RSV immunity results to compare_sensitivity

--- src/process_results/compare_sensitivity.R
+++ src/process_results/compare_sensitivity.R
@@ -14,6 +14,7 @@ res_dir_maih <- 'results/round2_fit/round2_3_fluH1_plus_B/'
 res_dir_noAH <- 'results/round2_fit/sens/no_ah/round2_3_fluH1_plus_B/'
 res_dir_sinusoidal <- 'results/round2_fit/sens/sinusoidal_forcing/round2_4_fluH1_plus_B/'
 res_dir_noint <- 'results/round2_fit/sens/no_int/round2_2_fluH1_plus_B/'
+res_dir_noRSVimmune <- 'results/round2_fit/sens/no_rsv_immune/round2_5_fluH1_plus_B/'
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -70,6 +71,7 @@ res_main <- load_and_format_mega_results(res_dir_maih, cond = 'Main')
 res_noah <- load_and_format_mega_results(res_dir_noAH, cond = 'No AH')
 res_sinusoidal <- load_and_format_mega_results(res_dir_sinusoidal, cond = 'Sinusoidal Forcing')
 res_noint <- load_and_format_mega_results(res_dir_noint, cond = 'No Interaction')
+res_noRSVimmune <- load_and_format_mega_results(res_dir_noRSVimmune, cond = 'No Immunity to RSV')
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -84,12 +86,15 @@ summary(res_sinusoidal %>%
           select(!contains('I10') & !contains('I20') & !contains('Ri') & !contains('R1') & !contains('R2'), -c(loglik, condition)))
 # summary(res_noint %>%
 #           select(!contains('I10') & !contains('I20') & !contains('Ri') & !contains('R1') & !contains('R2'), -c(loglik, condition)))
+summary(res_noRSVimmune %>%
+          select(!contains('I10') & !contains('I20') & !contains('Ri') & !contains('R1') & !contains('R2'), -c(loglik, condition)))
 
 # Compare log likelihoods:
 res <- bind_rows(res_main,
                  res_noah,
                  res_sinusoidal,
-                 res_noint)
+                 res_noint,
+                 res_noRSVimmune)
 
 p1 <- ggplot(data = res, aes(x = condition, y = loglik, group = condition)) + geom_jitter() + theme_classic()# + geom_boxplot()
 print(p1)
@@ -99,11 +104,13 @@ print(p1)
 print(2 * (min(res$loglik[res$condition == 'Main']) - max(res$loglik[res$condition == 'No AH'])) > qchisq(p = 0.95, df = 2))
 # print(2 * (min(res$loglik[res$condition == 'Main']) - max(res$loglik[res$condition == 'Sinusoidal Forcing'])) > qchisq(p = 0.95, df = 0))
 print(2 * (min(res$loglik[res$condition == 'Main']) - max(res$loglik[res$condition == 'No Interaction'])) > qchisq(p = 0.95, df = 4))
+print(2 * (min(res$loglik[res$condition == 'Main']) - max(res$loglik[res$condition == 'No Immunity to RSV'])) > qchisq(p = 0.95, df = 12))
 
 aic_main <- 2 * length(names(res_main %>% select(-c(loglik, condition)))) - 2 * max(res_main$loglik)
 aic_noah <- 2 * length(names(res_noah %>% select(-c(loglik, condition)))) - 2 * max(res_noah$loglik)
 aic_sinusoidal <- 2 * length(names(res_sinusoidal %>% select(-c(loglik, condition)))) - 2 * max(res_sinusoidal$loglik)
 aic_noint <- 2 * length(names(res_noint %>% select(-c(loglik, condition)))) - 2 * max(res_noint$loglik)
+aic_noRSVimmune <- 2 * length(names(res_noRSVimmune %>% select(-c(loglik, condition)))) - 2 * max(res_noRSVimmune$loglik)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
