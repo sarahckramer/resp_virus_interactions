67b6b0ee476dd812e9145778d3ceab13914ead82
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2024-01-25 12:06:51 +0100

    Update profile likelihood plots

--- src/generate_figures_SUPP.R
+++ src/generate_figures_SUPP.R
@@ -627,16 +627,6 @@ get_maxloglik_and_ci_cutoff <- function(res) {
   
 }
 
-res_proflik1 <- load_and_format_proflik_results(filepath = 'results/prof_lik_thetalambda1/',
-                                                prof_par = 'theta_lambda1',
-                                                shared_estpars = shared_estpars)
-res_proflik1 <- get_maxloglik_and_ci_cutoff(res_proflik1)
-
-res_proflik2 <- load_and_format_proflik_results(filepath = 'results/prof_lik_thetalambda2/',
-                                                prof_par = 'theta_lambda2',
-                                                shared_estpars = shared_estpars)
-res_proflik2 <- get_maxloglik_and_ci_cutoff(res_proflik2)
-
 res_proflik1_ZOOM <- load_and_format_proflik_results(filepath = 'results/prof_lik_thetalambda1ZOOM/',
                                                      prof_par = 'theta_lambda1',
                                                      shared_estpars = shared_estpars)
@@ -647,61 +637,42 @@ res_proflik2_ZOOM <- load_and_format_proflik_results(filepath = 'results/prof_li
                                                      shared_estpars = shared_estpars)
 res_proflik2_ZOOM <- get_maxloglik_and_ci_cutoff(res_proflik2_ZOOM)
 
-p9a_main <- ggplot(res_proflik1, # %>% filter(loglik > (max(loglik) - 100)), # allows for zooming
-                   aes(x = profpar, y = loglik)) +
+# res_proflik1_ZOOM %>%
+#   filter(loglik > (max(loglik) - 20))
+# res_proflik2_ZOOM %>%
+#   filter(loglik > (max(loglik) - 20))
+
+res_proflik1_ZOOM <- res_proflik1_ZOOM %>%
+  filter(profpar <= 0.011)
+res_proflik2_ZOOM <- res_proflik2_ZOOM %>%
+  filter(profpar <= 0.011)
+
+p9a <- ggplot(res_proflik1_ZOOM, aes(x = profpar, y = loglik)) +
   geom_point() + theme_classic() +
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 12),
         plot.tag = element_text(size = 22),
         plot.tag.position = c(0.02, 0.975)) +
-  geom_smooth(method = 'loess', span = 0.75, color = 'black') +
+  geom_smooth(method = 'loess', span = 0.7, color = 'black') +
   geom_hline(color = 'black', aes(yintercept = ci), linewidth = 1, lty = 2) +
   labs(x = bquote(theta[lambda*1]), y = 'Log-Likelihood', tag = 'A') +
   scale_x_continuous(n.breaks = 10) +
-  scale_y_continuous(n.breaks = 6)
-p9a_insert <- ggplot(res_proflik1_ZOOM, # %>% filter(loglik > (max(loglik) - 100)), # allows for zooming
-                     aes(x = profpar, y = loglik)) +
-  geom_point() + theme_classic() +
-  theme(axis.title = element_text(size = 14),
-        axis.text = element_text(size = 12)) +
-  geom_smooth(method = 'loess', span = 0.75, color = 'black') +
-  geom_hline(color = 'black', aes(yintercept = ci), linewidth = 1, lty = 2) +
-  labs(x = bquote(theta[lambda*1]), y = 'Log-Likelihood') +
-  scale_x_continuous(n.breaks = 5) +
-  scale_y_continuous(n.breaks = 6)
+  scale_y_continuous(limits = c(-9685, -9644), n.breaks = 6)
 
-p9b_main <- ggplot(res_proflik2, # %>% filter(loglik > (max(loglik) - 100)), # allows for zooming
-                   aes(x = profpar, y = loglik)) +
+p9b <- ggplot(res_proflik2_ZOOM, aes(x = profpar, y = loglik)) +
   geom_point() + theme_classic() +
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 12),
         plot.tag = element_text(size = 22),
         plot.tag.position = c(0.02, 0.975)) +
-  geom_smooth(method = 'loess', span = 0.75, color = 'black') +
+  geom_smooth(method = 'loess', span = 0.7, color = 'black') +
   geom_hline(color = 'black', aes(yintercept = ci), linewidth = 1, lty = 2) +
   labs(x = bquote(theta[lambda*2]), y = 'Log-Likelihood', tag = 'B') +
   scale_x_continuous(n.breaks = 10) +
-  scale_y_continuous(n.breaks = 6)
-p9b_insert <- ggplot(res_proflik2_ZOOM, # %>% filter(loglik > (max(loglik) - 100)), # allows for zooming
-                     aes(x = profpar, y = loglik)) +
-  geom_point() + theme_classic() +
-  theme(axis.title = element_text(size = 14),
-        axis.text = element_text(size = 12)) +
-  geom_smooth(method = 'loess', span = 0.75, color = 'black') +
-  geom_hline(color = 'black', aes(yintercept = ci), linewidth = 1, lty = 2) +
-  labs(x = bquote(theta[lambda*2]), y = 'Log-Likelihood') +
-  scale_x_continuous(n.breaks = 5) +
-  scale_y_continuous(n.breaks = 6)
-
-p9a <- ggdraw() +
-  draw_plot(p9a_main) +
-  draw_plot(p9a_insert, x = 0.11, y = 0.0955, width = 0.46, height = 0.43)
-p9b <- ggdraw() +
-  draw_plot(p9b_main) +
-  draw_plot(p9b_insert, x = 0.11, y = 0.0955, width = 0.46, height = 0.43)
+  scale_y_continuous(limits = c(-9685, -9644), n.breaks = 6)
 
 fig9s <- arrangeGrob(p9a, p9b, nrow = 1)
-# ggsave('results/plots/figures_for_manuscript/supp/FigureS9.svg', width = 17, height = 6, fig9s)
+# ggsave('results/plots/figures_for_manuscript/supp/FigureS9.svg', width = 14, height = 5, fig9s)
 
 rm(fig9s, p9a, p9b, p9a_main, p9a_insert, p9b_main, p9b_insert,
    res_proflik1, res_proflik1_ZOOM, res_proflik2, res_proflik2_ZOOM)
