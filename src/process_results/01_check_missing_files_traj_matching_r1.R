d4d9d5a9a1496997d3d5a502e1d9ee13cda7e661
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2024-02-09 08:44:37 +0100

    Process and plot round1 results

--- src/process_results/01_check_missing_files_traj_matching_r1.R
+++ src/process_results/01_check_missing_files_traj_matching_r1.R
@@ -8,6 +8,7 @@ library(testthat)
 
 # Get cluster environmental variables:
 fit_canada <- as.logical(Sys.getenv("FITCANADA")); print(fit_canada)
+fit_us <- as.logical(Sys.getenv("FITUS")); print(fit_us)
 
 # Set size of parameter start space:
 sobol_size <- 500
@@ -15,7 +16,7 @@ sobol_size <- 500
 # Get list of completed runs for each virus:
 res_files <- list.files(path = 'results', pattern = 'res_', full.names = TRUE)
 
-if (fit_canada) {
+if (fit_canada | fit_us) {
   check_list <- list.files(path = 'results/', pattern = 'flu_r')
 } else {
   check_list <- list.files(path = 'results/', pattern = 'flu_h1_plus_b_r')
@@ -24,49 +25,92 @@ if (fit_canada) {
 # Get vector of seasons for which fitting was done:
 if (fit_canada) {
   all_yrs <- unique(str_sub(check_list, 13, 18))
+} else if (fit_us) {
+  all_yrs <- unique(str_sub(check_list, 15, 20))
 } else {
   all_yrs <- unique(str_sub(check_list, 23, 28))
 }
 print(all_yrs)
 print(length(all_yrs))
 
+# If US, get vector of regions for which fitting was done:
+if (fit_us) {
+  all_regions <- unique(str_sub(check_list, 13, 13))
+  print(all_regions)
+  print(length(all_regions))
+} else {
+  all_regions <- c()
+}
+
 # Check for complete results:
-if (length(check_list) != sobol_size * length(all_yrs) & length(check_list) > 0) {
+if (length(check_list) != sobol_size * length(all_yrs) * length(all_regions) & length(check_list) > 0) {
   
   for (yr in all_yrs) {
-    temp_list <- check_list[str_detect(check_list, pattern = yr)]
     
-    if (length(temp_list) != sobol_size) {
+    if (length(all_regions) > 1) {
       
-      if (fit_canada) {
+      for (region in all_regions) {
         
-        completed_runs <- str_split(temp_list, '_') %>%
-          lapply(., function(ix) {ix[5]}) %>%
-          unlist() %>%
-          str_split(fixed('.')) %>%
-          lapply(., function(ix) {ix[1]}) %>%
-          unlist() %>%
-          as.numeric()
+        temp_list <- check_list[str_detect(check_list, pattern = yr) & str_detect(check_list, pattern = region)]
         
-      } else {
+        if (length(temp_list) != sobol_size) {
+          
+          completed_runs <- str_split(temp_list, '_') %>%
+            lapply(., function(ix) {ix[6]}) %>%
+            unlist() %>%
+            str_split(fixed('.')) %>%
+            lapply(., function(ix) {ix[1]}) %>%
+            unlist() %>%
+            as.numeric()
+          
+          missing_runs <- which(!(c(1:sobol_size) %in% completed_runs))
+          
+          for (run in missing_runs) {
+            print(paste0('For flu_h1_plus_b/RSV in Region ', region, ' and ', yr, ', missing run: ', run))
+          }
+          
+        }
         
-        completed_runs <- str_split(temp_list, '_') %>%
-          lapply(., function(ix) {ix[8]}) %>%
-          unlist() %>%
-          str_split(fixed('.')) %>%
-          lapply(., function(ix) {ix[1]}) %>%
-          unlist() %>%
-          as.numeric()
-
       }
       
-      missing_runs <- which(!(c(1:sobol_size) %in% completed_runs))
+    } else {
       
-      for (run in missing_runs) {
-        print(paste0('For flu_h1_plus_b/RSV in ', yr, ', missing run: ', run))
+      temp_list <- check_list[str_detect(check_list, pattern = yr)]
+      
+      if (length(temp_list) != sobol_size) {
+        
+        if (fit_canada | fit_us) {
+          
+          completed_runs <- str_split(temp_list, '_') %>%
+            lapply(., function(ix) {ix[5]}) %>%
+            unlist() %>%
+            str_split(fixed('.')) %>%
+            lapply(., function(ix) {ix[1]}) %>%
+            unlist() %>%
+            as.numeric()
+          
+        } else {
+          
+          completed_runs <- str_split(temp_list, '_') %>%
+            lapply(., function(ix) {ix[8]}) %>%
+            unlist() %>%
+            str_split(fixed('.')) %>%
+            lapply(., function(ix) {ix[1]}) %>%
+            unlist() %>%
+            as.numeric()
+          
+        }
+        
+        missing_runs <- which(!(c(1:sobol_size) %in% completed_runs))
+        
+        for (run in missing_runs) {
+          print(paste0('For flu_h1_plus_b/RSV in ', yr, ', missing run: ', run))
+        }
+        
       }
       
     }
+    
   }
 }
 
