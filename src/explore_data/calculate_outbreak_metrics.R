bb65ba15e612c3e72c1144ec3a43d1853a8032a0
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2024-03-05 13:27:27 +0100

    Add "explore data" code for the US

--- src/explore_data/calculate_outbreak_metrics.R
+++ src/explore_data/calculate_outbreak_metrics.R
@@ -7,240 +7,143 @@ library(tidyverse)
 library(testthat)
 
 # Read in data:
-hk_dat <- read_rds('data/formatted/dat_hk_byOutbreak.rds')
+hk_dat <- read_rds('data/formatted/dat_hk_byOutbreak.rds')$h1_plus_b_rsv
 can_dat <- read_csv('data/formatted/dat_canada.csv')
+us_dat <- read_rds('data/formatted/dat_us_byRegion.rds')$'Region 8'
 
-# Compile relevant data:
-hk_dat <- hk_dat$h1_rsv %>%
-  rename(n_h1 = n_P1,
-         n_rsv = n_P2) %>%
-  select(time, Year, Week, season, n_h1, n_rsv, n_T, GOPC, pop) %>%
-  full_join(hk_dat$b_rsv %>%
-              rename(n_b = n_P1,
-                     n_rsv = n_P2) %>%
-              select(time, season, n_b, n_rsv, n_T, GOPC, pop),
-            by = c('time', 'season')) %>%
-  full_join(hk_dat$h1_plus_b_rsv %>%
-              rename(n_h1_plus_b = n_P1,
-                     n_rsv = n_P2) %>%
-              select(time, season, n_h1_plus_b, n_rsv, n_T, GOPC, pop),
-            by = c('time', 'season'))
-
-expect_true(all.equal(hk_dat$n_rsv.x, hk_dat$n_rsv.y))
-expect_true(all.equal(hk_dat$n_T.x, hk_dat$n_T.y))
-expect_true(all.equal(hk_dat$GOPC.x, hk_dat$GOPC.y))
-expect_true(all.equal(hk_dat$pop.x, hk_dat$pop.y))
-
-expect_true(all.equal(hk_dat$n_rsv, hk_dat$n_rsv.y))
-expect_true(all.equal(hk_dat$n_T, hk_dat$n_T.y))
-expect_true(all.equal(hk_dat$GOPC, hk_dat$GOPC.y))
-expect_true(all.equal(hk_dat$pop, hk_dat$pop.y))
-
+# Format and join:
 hk_dat <- hk_dat %>%
-  select(time:n_h1, n_b, n_h1_plus_b:pop) %>%
-  arrange(season)
+  select(time, season, pop, n_T:n_P2) %>%
+  mutate(n_T1 = n_T,
+         n_T2 = n_T) %>%
+  select(time:pop, n_T1:n_T2, n_P1:n_P2) %>%
+  mutate(loc = 'hk')
 
-# Calculate peak timing and attack rate metrics:
-hk_metrics_pt_ar <- hk_dat %>%
-  pivot_longer(n_h1:n_rsv, names_to = 'vir', values_to = 'number') %>%
-  mutate(prop = number / pop,
-         prop_pos = number / n_T) %>%
-  select(-GOPC) %>%
-  group_by(vir, season) %>%
-  summarise(pt = which.max(prop_pos) + 45,
-            ar = sum(prop, na.rm = TRUE),
-            ar_prop = sum(number, na.rm = TRUE) / sum(n_T, na.rm = TRUE))
+can_dat <- can_dat %>%
+  select(time, season, pop, n_T1:n_T2, n_P1:n_P2) %>%
+  mutate(loc = 'can')
 
-can_metrics_pt_ar <- can_dat %>%
-  select(-c(n_test_rhino, rhino)) %>%
+us_dat <- us_dat %>%
+  select(time, season, pop, n_T1:n_P2) %>%
+  mutate(loc = 'us')
+
+dat <- rbind(hk_dat, can_dat, us_dat)
+rm(hk_dat, can_dat, us_dat)
+
+# Calculate peak timing:
+metrics_pt <- dat %>%
+  mutate(prop1 = n_P1 / n_T1,
+         prop2 = n_P2 / n_T2) %>%
+  pivot_longer(prop1:prop2, names_to = 'vir', values_to = 'prop_pos') %>%
+  group_by(vir, loc, season) %>%
+  summarise(pt = which.max(prop_pos)) %>%
+  mutate(pt = if_else(loc == 'hk', pt + 45, pt),
+         pt = if_else(loc == 'can', pt + 34, pt),
+         pt = if_else(loc == 'us', pt + 39, pt)) %>%
+  mutate(vir = if_else(vir == 'prop1', 'influenza', 'rsv'))
+
+# Calculate attack rates:
+metrics_ar <- dat %>%
   pivot_longer(n_P1:n_P2, names_to = 'vir', values_to = 'number') %>%
   pivot_longer(n_T1:n_T2, names_to = 'vir1', values_to = 'n_T') %>%
-  mutate(vir = str_sub(vir, 4),
-         vir1 = str_sub(vir1, 4)) %>%
+  mutate(vir = str_sub(vir, -1),
+         vir1 = str_sub(vir1, -1)) %>%
   filter(vir == vir1) %>%
-  select(-vir1, i_ILI) %>%
-  mutate(vir = if_else(vir == 1, 'influenza', 'rsv')) %>%
-  mutate(prop = number / pop,
-         prop_pos = number / n_T) %>%
-  group_by(vir, season) %>%
-  summarise(pt = which.max(prop_pos) + 34,
-            ar = sum(prop, na.rm = TRUE),
-            ar_prop = sum(number, na.rm = TRUE) / sum(n_T, na.rm = TRUE))
+  mutate(prop = number / pop) %>%
+  group_by(vir, loc, season) %>%
+  summarise(ar = sum(prop, na.rm = TRUE),
+            ar_prop = sum(number, na.rm = TRUE) / sum(n_T, na.rm = TRUE)) %>%
+  mutate(vir = if_else(vir == '1', 'influenza', 'rsv'))
 
 # Calculate peak timing differences:
-hk_metrics_pt_diff <- hk_metrics_pt_ar %>%
-  select(-c(ar, ar_prop)) %>%
+metrics_pt_diff <- metrics_pt %>%
   pivot_wider(names_from = vir, values_from = pt) %>%
-  mutate(n_h1 = n_h1 - n_rsv,
-         n_b = n_b - n_rsv,
-         n_h1_plus_b = n_h1_plus_b - n_rsv) %>%
-  select(-n_rsv) %>%
-  pivot_longer(n_b:n_h1_plus_b, names_to = 'vir', values_to = 'pt_diff')
-
-can_metrics_pt_diff <- can_metrics_pt_ar %>%
-  select(-c(ar, ar_prop)) %>%
-  pivot_wider(names_from = vir, values_from = pt) %>%
-  mutate(pt_diff = influenza - rsv,
-         vir = 'influenza') %>%
-  select(season, vir, pt_diff)
+  mutate(pt_diff = influenza - rsv) %>%
+  select(loc:season, pt_diff) %>%
+  mutate(vir = 'influenza')
 
 # Calculate the number of weeks containing 75% of reported cases:
-hk_metrics_conc <- NULL
-for (yr in unique(hk_dat$season)) {
+metrics_conc <- NULL
+for (lc in unique(dat$loc)) {
   
-  hk_dat_temp <- hk_dat %>%
-    filter(season == yr)
+  dat_loc <- dat %>%
+    filter(loc == lc)
   
-  for (vir in c('n_h1', 'n_b', 'n_h1_plus_b', 'n_rsv')) {
+  for (yr in unique(dat_loc$season)) {
     
-    case_counts_temp <- hk_dat_temp %>% pull(vir)
-    case_counts_temp[is.na(case_counts_temp)] <- 0
+    dat_temp <- dat_loc %>%
+      filter(season == yr)
     
-    target_sum <- sum(case_counts_temp)
-    sum_cases <- 0
-    which_weeks <- c()
-    
-    while (sum_cases < 0.75 * target_sum) {
+    for (vir in c('n_P1', 'n_P2')) {
       
-      which_max <- which.max(case_counts_temp)
-      max_val <- case_counts_temp[which_max]
+      case_counts_temp <- dat_temp %>% pull(vir)
+      case_counts_temp[is.na(case_counts_temp)] <- 0
       
-      sum_cases <- sum_cases + max_val
-      which_weeks <- c(which_weeks, which_max)
+      target_sum <- sum(case_counts_temp)
+      sum_cases <- 0
+      which_weeks <- c()
       
-      case_counts_temp[which_max] <- 0
+      while (sum_cases < 0.75 * target_sum) {
+        
+        which_max <- which.max(case_counts_temp)
+        max_val <- case_counts_temp[which_max]
+        
+        sum_cases <- sum_cases + max_val
+        which_weeks <- c(which_weeks, which_max)
+        
+        case_counts_temp[which_max] <- 0
+        
+      }
+      
+      metrics_conc <- rbind(metrics_conc,
+                            c(lc,
+                              yr,
+                              vir,
+                              length(which_weeks),
+                              min(which_weeks) + length(which_weeks) - 1 == max(which_weeks)))
+      
       
     }
-    
-    hk_metrics_conc <- rbind(hk_metrics_conc,
-                             c(vir,
-                               yr,
-                               length(which_weeks),
-                               min(which_weeks) + length(which_weeks) - 1 == max(which_weeks)))
     
   }
   
-  rm(case_counts_temp, target_sum, sum_cases, which_weeks, which_max, max_val)
-  
 }
-rm(hk_dat_temp, yr, vir)
+rm(dat_loc, dat_temp, case_counts_temp, lc, max_val, sum_cases, target_sum, vir, which_max, which_weeks, yr)
 
-hk_metrics_conc <- hk_metrics_conc %>%
+metrics_conc <- metrics_conc %>%
   as_tibble() %>%
-  rename('vir' = 'V1',
+  rename('loc' = 'V1',
          'season' = 'V2',
-         'duration' = 'V3',
-         'consecutive' = 'V4')
-
-can_metrics_conc <- NULL
-for (yr in unique(can_dat$season)) {
-  
-  can_dat_temp <- can_dat %>%
-    filter(season == yr)
-  
-  for (vir in c('influenza', 'rsv')) {
-    
-    if (vir == 'influenza') {
-      case_counts_temp <- can_dat_temp %>% pull(n_P1)
-    } else if (vir == 'rsv') {
-      case_counts_temp <- can_dat_temp %>% pull(n_P2)
-    }
-    
-    target_sum <- sum(case_counts_temp)
-    sum_cases <- 0
-    which_weeks <- c()
-    
-    while (sum_cases < 0.75 * target_sum) {
-      
-      which_max <- which.max(case_counts_temp)
-      max_val <- case_counts_temp[which_max]
-      
-      sum_cases <- sum_cases + max_val
-      which_weeks <- c(which_weeks, which_max)
-      
-      case_counts_temp[which_max] <- 0
-      
-    }
-    
-    can_metrics_conc <- rbind(can_metrics_conc,
-                              c(vir,
-                                yr,
-                                length(which_weeks),
-                                min(which_weeks) + length(which_weeks) - 1 == max(which_weeks)))
-    
-  }
-  
-  rm(case_counts_temp, target_sum, sum_cases, which_weeks, which_max, max_val)
-  
-}
-rm(can_dat_temp, yr, vir)
-
-can_metrics_conc <- can_metrics_conc %>%
-  as_tibble() %>%
-  rename('vir' = 'V1',
-         'season' = 'V2',
-         'duration' = 'V3',
-         'consecutive' = 'V4')
+         'vir' = 'V3',
+         'duration' = 'V4',
+         'consecutive' = 'V5') %>%
+  mutate(vir = if_else(vir == 'n_P1', 'influenza', 'rsv'))
 
 # Join all metrics:
-metrics_hk <- hk_metrics_pt_ar %>%
-  left_join(hk_metrics_pt_diff, by = c('vir', 'season')) %>%
-  left_join(hk_metrics_conc, by = c('vir', 'season')) %>%
-  select(vir:season, ar:ar_prop, pt, pt_diff, duration:consecutive) %>%
+metrics <- metrics_pt %>%
+  left_join(metrics_ar, by = c('loc', 'season', 'vir')) %>%
+  left_join(metrics_pt_diff, by = c('loc', 'season', 'vir')) %>%
+  left_join(metrics_conc, by = c('loc', 'season', 'vir')) %>%
+  select(loc:season, vir, ar:ar_prop, pt, pt_diff, duration:consecutive) %>%
   mutate(duration = as.numeric(duration),
          consecutive = as.numeric(as.logical(consecutive))) %>%
   mutate(ar = ar * 100,
          ar_prop = ar_prop * 100)
-rm(hk_metrics_pt_ar, hk_metrics_pt_diff, hk_metrics_conc)
 
-metrics_can <- can_metrics_pt_ar %>%
-  left_join(can_metrics_pt_diff, by = c('vir', 'season')) %>%
-  left_join(can_metrics_conc, by = c('vir', 'season')) %>%
-  select(vir:season, ar:ar_prop, pt, pt_diff, duration:consecutive) %>%
-  mutate(duration = as.numeric(duration),
-         consecutive = as.numeric(as.logical(consecutive))) %>%
-  mutate(ar = ar * 100,
-         ar_prop = ar_prop * 100)
-rm(can_metrics_pt_ar, can_metrics_pt_diff, can_metrics_conc)
+expect_true(nrow(metrics) == nrow(metrics_pt))
+expect_true(nrow(metrics) == nrow(metrics_ar))
+expect_true(nrow(metrics) == nrow(metrics_conc))
+expect_true(nrow(metrics) == nrow(metrics_pt_diff) * 2)
 
-# Combine
-metrics <- metrics_hk %>%
-  filter(vir %in% c('n_h1_plus_b', 'n_rsv')) %>%
-  mutate(vir = if_else(vir == 'n_h1_plus_b', 'influenza', 'rsv'),
-         location = 'hk') %>%
-  bind_rows(metrics_can %>%
-              mutate(location = 'can'))
+rm(metrics_pt, metrics_ar, metrics_pt_diff, metrics_conc)
 
 # Print values:
 metrics <- metrics %>%
   pivot_longer(ar:consecutive, names_to = 'metric', values_to = 'val')
-metrics_hk_full <- metrics_hk %>%
-  pivot_longer(ar:consecutive, names_to = 'metric', values_to = 'val')
-rm(metrics_hk, metrics_can)
 
 metrics %>%
   filter(vir == 'influenza') %>%
-  group_by(location, metric) %>%
-  summarise(mean = mean(val),
-            median = median(val),
-            min = min(val),
-            max = max(val),
-            sum = sum(val)) %>%
-  print()
-
-metrics %>%
-  filter(vir == 'influenza', metric == 'pt') %>%
-  mutate(val = if_else(season == 's16-17', val - 53, val - 52)) %>%
-  group_by(location) %>%
-  summarise(min = min(val),
-            median = median(val),
-            mean = mean(val),
-            max = max(val)) %>%
-  print()
-
-metrics %>%
-  filter(vir == 'rsv') %>%
-  group_by(location, metric) %>%
+  group_by(metric, loc) %>%
   summarise(mean = mean(val),
             median = median(val),
             min = min(val),
@@ -250,8 +153,32 @@ metrics %>%
 
 metrics %>%
   filter(vir == 'rsv', metric == 'pt') %>%
-  mutate(val = if_else(season == 's16-17', val - 53, val - 52)) %>%
-  group_by(location) %>%
+  mutate(val = val - 52) %>%
+  mutate(val = if_else(season == 's16-17' & loc == 'hk', val - 1, val),
+         val = if_else(season == 's14-15' & loc == 'us', val - 1, val)) %>%
+  group_by(loc) %>%
+  summarise(min = min(val),
+            median = median(val),
+            mean = mean(val),
+            max = max(val)) %>%
+  print()
+
+metrics %>%
+  filter(vir == 'rsv') %>%
+  group_by(metric, loc) %>%
+  summarise(mean = mean(val),
+            median = median(val),
+            min = min(val),
+            max = max(val),
+            sum = sum(val)) %>%
+  print()
+
+metrics %>%
+  filter(vir == 'rsv', metric == 'pt') %>%
+  mutate(val = val - 52) %>%
+  mutate(val = if_else(season == 's16-17' & loc == 'hk', val - 1, val),
+         val = if_else(season == 's14-15' & loc == 'us', val - 1, val)) %>%
+  group_by(loc) %>%
   summarise(min = min(val),
             median = median(val),
             mean = mean(val),
@@ -260,20 +187,12 @@ metrics %>%
 
 # Plot "realistic" values:
 p1 <- ggplot(data = metrics %>% filter(metric != 'consecutive'),
-       aes(x = vir, y = val)) +
+             aes(x = loc, y = val)) +
   geom_violin(fill = 'gray90') +
-  facet_grid(metric ~ location, scales = 'free_y') +
+  facet_grid(metric ~ vir, scales = 'free_y') +
   theme_classic() +
   labs(x = 'Virus', y = 'Value')
 print(p1)
-
-p2 <- ggplot(data = metrics_hk_full %>% filter(metric != 'consecutive'),
-             aes(x = vir, y = val)) +
-  geom_violin(fill = 'gray90') +
-  facet_wrap(~ metric, scales = 'free_y') +
-  theme_classic() +
-  labs(x = 'Virus', y = 'Value')
-print(p2)
 
 # Clean up:
 rm(list = ls())
--- src/explore_data/explore_data_smoothness.R
+++ src/explore_data/explore_data_smoothness.R
@@ -9,13 +9,11 @@ library(tidyverse)
 dat_hk <- read_csv('data/formatted/dat_hk.csv')
 dat_hk <- dat_hk %>%
   filter(Year < 2020 & !(Year == 2019 & Week > 45)) %>%
-  select(Time:n_h1, n_b:n_rsv, GOPC) %>%
-  mutate(prop_h1 = n_h1 / n_samp * 100,
-         prop_b = n_b / n_samp * 100,
-         prop_h1_plus_b = n_h1_b / n_samp * 100,
+  select(Time:n_samp, n_h1_b:n_rsv, GOPC) %>%
+  mutate(prop_flu = n_h1_b / n_samp * 100,
          prop_rsv = n_rsv / n_samp * 100) %>%
   select(-n_samp) %>%
-  select(Time:n_rsv, prop_h1:prop_rsv, GOPC)
+  select(Time:n_rsv, prop_flu:prop_rsv, GOPC)
 
 dat_can <- read_csv('data/formatted/dat_canada.csv')
 dat_can <- dat_can %>%
@@ -25,27 +23,38 @@ dat_can <- dat_can %>%
   select(-c(n_T1, n_T2)) %>%
   select(time:n_P2, prop_P1:prop_P2, i_ILI)
 
+dat_us <- read_rds('data/formatted/dat_us_byRegion.rds')$'Region 8'
+dat_us <- dat_us %>%
+  select(time, year:week, n_T1:i_ILI) %>%
+  mutate(prop_P1 = n_P1 / n_T1 * 100,
+         prop_P2 = n_P2 / n_T2 * 100) %>%
+  select(-c(n_T1, n_T2)) %>%
+  select(time:n_P2, prop_P1:prop_P2, i_ILI)
+
 # Calculate lag-one autocorrelation for raw observation numbers:
-cor(dat_hk$n_h1[-length(dat_hk$n_h1)], dat_hk$n_h1[-1])
-cor(dat_hk$n_b[-length(dat_hk$n_b)], dat_hk$n_b[-1])
 cor(dat_hk$n_h1_b[-length(dat_hk$n_h1_b)], dat_hk$n_h1_b[-1])
 cor(dat_hk$n_rsv[-length(dat_hk$n_rsv)], dat_hk$n_rsv[-1])
 
 cor(dat_can$n_P1[-length(dat_can$n_P1)], dat_can$n_P1[-1])
 cor(dat_can$n_P2[-length(dat_can$n_P2)], dat_can$n_P2[-1])
 
+cor(dat_us$n_P1[-length(dat_us$n_P1)], dat_us$n_P1[-1])
+cor(dat_us$n_P2[-length(dat_us$n_P2)], dat_us$n_P2[-1], use = 'pairwise.complete.obs')
+
 # Calculate lag-one autocorrelation for positivity rates:
-cor(dat_hk$prop_h1[-length(dat_hk$prop_h1)], dat_hk$prop_h1[-1])
-cor(dat_hk$prop_b[-length(dat_hk$prop_b)], dat_hk$prop_b[-1])
-cor(dat_hk$prop_h1_plus_b[-length(dat_hk$prop_h1_plus_b)], dat_hk$prop_h1_plus_b[-1])
+cor(dat_hk$prop_flu[-length(dat_hk$prop_flu )], dat_hk$prop_flu [-1])
 cor(dat_hk$prop_rsv[-length(dat_hk$prop_rsv)], dat_hk$prop_rsv[-1])
 
 cor(dat_can$prop_P1[-length(dat_can$prop_P1)], dat_can$prop_P1[-1])
 cor(dat_can$prop_P2[-length(dat_can$prop_P2)], dat_can$prop_P2[-1])
 
+cor(dat_us$prop_P1[-length(dat_us$prop_P1)], dat_us$prop_P1[-1])
+cor(dat_us$prop_P2[-length(dat_us$prop_P2)], dat_us$prop_P2[-1], use = 'pairwise.complete.obs')
+
 # Calculate lag-one autocorrelation for ILI:
 cor(dat_hk$GOPC[-length(dat_hk$GOPC)], dat_hk$GOPC[-1])
 cor(dat_can$i_ILI[-length(dat_can$i_ILI)], dat_can$i_ILI[-1])
+cor(dat_us$i_ILI[-length(dat_us$i_ILI)], dat_us$i_ILI[-1])
 
 # Clean up:
 rm(list = ls())
