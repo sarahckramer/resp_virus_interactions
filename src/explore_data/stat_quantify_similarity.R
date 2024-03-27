f65eee9248178062f37f029ed821db5b7ac25b63
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2024-03-27 11:37:53 +0100

    Subtype-specific correlations

--- src/explore_data/stat_quantify_similarity.R
+++ src/explore_data/stat_quantify_similarity.R
@@ -9,12 +9,15 @@ library(tidyverse)
 dat_hk <- read_csv('data/formatted/dat_hk.csv')
 dat_hk <- dat_hk %>%
   filter(Year < 2020 & !(Year == 2019 & Week > 45)) %>%
-  select(Time:n_samp, n_h1_b:n_rsv, n_rhino_est_rnd, GOPC) %>%
-  mutate(prop_h1_plus_b = n_h1_b / n_samp * 100,
+  select(Time:n_samp, n_h1:n_rsv, n_rhino_est_rnd, GOPC) %>%
+  mutate(prop_h1 = n_h1 / n_samp * 100,
+         prop_h3 = n_h3 / n_samp * 100,
+         prop_b = n_b / n_samp * 100,
+         prop_h1_plus_b = n_h1_b / n_samp * 100,
          prop_rsv = n_rsv / n_samp * 100,
          prop_rhino = n_rhino_est_rnd / n_samp * 100) %>%
   select(-n_samp) %>%
-  select(Time:n_rhino_est_rnd, prop_h1_plus_b:prop_rhino, GOPC)
+  select(Time:n_rhino_est_rnd, prop_h1:prop_rhino, GOPC)
 
 dat_can <- read_csv('data/formatted/dat_canada.csv')
 dat_can <- dat_can %>%
